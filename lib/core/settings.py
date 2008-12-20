996a872e5198e5ee556bb8643c7874fb1d6e5e9e
Bernardo Damele <bernardo.damele@gmail.com>
2008-12-20 13:23:26 +0000

    We are already on sqlmap 0.6.4 release candidate 1..

--- lib/core/settings.py
+++ lib/core/settings.py
@@ -30,7 +30,7 @@ import sys
 
 
 # sqlmap version and site
-VERSION            = "0.6.3"
+VERSION            = "0.6.4-rc1"
 VERSION_STRING     = "sqlmap/%s" % VERSION
 SITE               = "http://sqlmap.sourceforge.net"
 
