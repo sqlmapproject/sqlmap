5560f0b68ac3ef8c10bf248f74fb4f3b16de1d99
Bernardo Damele <bernardo.damele@gmail.com>
2009-01-12 21:35:38 +0000

    Updated the copyright

--- lib/parse/html.py
+++ lib/parse/html.py
@@ -5,7 +5,7 @@ $Id$
 
 This file is part of the sqlmap project, http://sqlmap.sourceforge.net.
 
-Copyright (c) 2006-2008 Bernardo Damele A. G. <bernardo.damele@gmail.com>
+Copyright (c) 2006-2009 Bernardo Damele A. G. <bernardo.damele@gmail.com>
                         and Daniele Bellucci <daniele.bellucci@gmail.com>
 
 sqlmap is free software; you can redistribute it and/or modify it under
