b2b2ec8a26586144a678d0ab7d55d20d8cb0a290
Bernardo Damele <bernardo.damele@gmail.com>
2009-07-24 23:20:57 +0000

    Preparing to release sqlmap 0.7 stable

--- lib/core/settings.py
+++ lib/core/settings.py
@@ -30,7 +30,7 @@ import sys
 
 
 # sqlmap version and site
-VERSION            = "0.7rc5"
+VERSION            = "0.7"
 VERSION_STRING     = "sqlmap/%s" % VERSION
 SITE               = "http://sqlmap.sourceforge.net"
 
