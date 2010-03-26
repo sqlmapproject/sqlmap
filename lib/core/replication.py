0aa8f7309b35450fcbb8667b7fbd45ac47f78a56
Miroslav Stampar <miroslav.stampar@gmail.com>
2010-03-26 20:23:08 +0000

    added copyright notice and keywords

--- lib/core/replication.py
+++ lib/core/replication.py
@@ -1,5 +1,29 @@
-import sqlite3
+#!/usr/bin/env python
 
+"""
+$Id$
+
+This file is part of the sqlmap project, http://sqlmap.sourceforge.net.
+
+Copyright (C) 2010  Miroslav Stampar, Bernardo Damele A. G.
+email(s): miroslav.stampar@gmail.com, bernardo.damele@gmail.com
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+import sqlite3
 
 class Replication:
     def __init__(self, dbpath):
