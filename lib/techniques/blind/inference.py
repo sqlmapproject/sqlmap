d8e5585c6620a4b600ecbba6b5a68d14cba36e87
Miroslav Stampar <miroslav.stampar@gmail.com>
2010-04-29 08:30:29 +0000

    fixed a bug reported by Mosk Dmitri (infoMsg UnboundLocalError)

--- lib/techniques/blind/inference.py
+++ lib/techniques/blind/inference.py
@@ -304,6 +304,7 @@ def bisection(payload, expression, length=None, charsetType=None, firstChar=None
             conf.threadContinue = False
             raise
 
+        infoMsg = None
         # If we have got one single character not correctly fetched it
         # can mean that the connection to the target url was lost
         if None in value:
