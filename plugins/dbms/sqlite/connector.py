e810fe7b0b0b11ff74dbf14e3f6b6b06635cfbd2
Miroslav Stampar <miroslav.stampar@gmail.com>
2010-08-31 13:37:53 +0000

    no need for obsolete (and hard to find) sqlite module when sqlite3 handles both database versions

--- plugins/dbms/sqlite/connector.py
+++ plugins/dbms/sqlite/connector.py
@@ -23,7 +23,6 @@ Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
 try:
-    import sqlite
     import sqlite3
 except ImportError, _:
     pass
@@ -54,15 +53,9 @@ class Connector(GenericConnector):
         self.checkFileDb()
 
         try:
-            self.connector = sqlite.connect(database=self.db, check_same_thread=False, timeout=conf.timeout)
-        except (sqlite.DatabaseError, sqlite.OperationalError), _:
-            errMsg = "unable to connect using SQLite 2 library, trying with SQLite 3"
-            logger.error(errMsg)
-
-            try:
-                self.connector = sqlite3.connect(database=self.db, check_same_thread=False, timeout=conf.timeout)
-            except (sqlite.DatabaseError, sqlite.OperationalError), msg:
-                raise sqlmapConnectionException, msg[0]
+            self.connector = sqlite3.connect(database=self.db, check_same_thread=False, timeout=conf.timeout)
+        except (sqlite3.DatabaseError, sqlite3.OperationalError), msg:
+            raise sqlmapConnectionException, msg[0]
 
         self.setCursor()
         self.connected()
