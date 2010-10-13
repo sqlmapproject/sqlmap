4b641afc46f0f547f217df14b105cb85a9ab9a0b
Miroslav Stampar <miroslav.stampar@gmail.com>
2010-10-13 14:37:11 +0000

    minor update

--- tamper/doubleencode.py
+++ tamper/doubleencode.py
@@ -1,11 +1,15 @@
 import re
 
 from lib.core.convert import urlencode
+from lib.core.exception import sqlmapUnsupportedFeatureException
 
 """
 Tampering value -> urlencode(value)
 """
 def tamper(place, value):
     if value:
-        value = urlencode(value)
+        if place != "URI":
+            value = urlencode(value)
+        else:
+            raise sqlmapUnsupportedFeatureException, "can't use tampering module 'doubleencode.py' with 'URI' type injections"
     return value
