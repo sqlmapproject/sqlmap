1a9aabf49dc6c557aaecb4e7895ef7d394717ec5
Bernardo Damele <bernardo.damele@gmail.com>
2010-10-18 10:40:05 +0000

    Minor fix

--- tamper/charunicodeencode.py
+++ tamper/charunicodeencode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-$Id: charencode.py 2035 2010-10-16 21:33:15Z inquisb $
+$Id$
 
 Copyright (c) 2006-2010 sqlmap developers (http://sqlmap.sourceforge.net/)
 See the file 'doc/COPYING' for copying permission
@@ -13,7 +13,7 @@ from lib.core.exception import sqlmapUnsupportedFeatureException
 
 def tamper(place, value):
     """
-    Replaces value with urlencode of non-encoded chars in value
+    Replaces value with unicode-urlencode of non-encoded chars in value
     Example: 'SELECT%20FIELD%20FROM%20TABLE' becomes '%u0053%u0045%u004c%u0045%u0043%u0054%u0020%u0046%u0049%u0045%u004c%u0044%u0020%u0046%u0052%u004f%u004d%u0020%u0054%u0041%u0042%u004c%u0045'
     """
 
@@ -26,7 +26,7 @@ def tamper(place, value):
 
             while i < len(value):
                 if value[i] == '%' and (i < len(value) - 2) and value[i+1] in string.hexdigits and value[i+2] in string.hexdigits:
-                    retVal += value[i:i+3]
+                    retVal += "%%u00%s" % value[i+1:i+3]
                     i += 3
                 else:
                     retVal += '%%u00%X' % ord(value[i])
