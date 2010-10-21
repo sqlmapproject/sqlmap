fe3967bdec3d8add59aad69ebd18458ae5365f40
Miroslav Stampar <miroslav.stampar@gmail.com>
2010-10-21 13:28:29 +0000

    fix for --privileges (on MSSql --privileges returned exception)

--- plugins/dbms/sqlite/enumeration.py
+++ plugins/dbms/sqlite/enumeration.py
@@ -41,7 +41,7 @@ class Enumeration(GenericEnumeration):
 
         return {}
 
-    def getPrivileges(self, _):
+    def getPrivileges(self, *args):
         warnMsg = "on SQLite it is not possible to enumerate the user privileges"
         logger.warn(warnMsg)
 
