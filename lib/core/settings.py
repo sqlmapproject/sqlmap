f762f32de8a8835424289ba90b7e3dad9ceca217
Miroslav Stampar <miroslav.stampar@gmail.com>
2011-01-02 13:00:04 +0000

    bug fix for proper --parse-errors on .aspx pages

--- lib/core/settings.py
+++ lib/core/settings.py
@@ -165,7 +165,7 @@ SQL_STATEMENTS      = {
                      }
 
 ERROR_PARSING_REGEXES = (   
-                            r"<b>[^<]*(fatal|error|warning|exception)[^<]*</b>:?\s+(?P<result>.+?)<br\s*/?\s*>", 
+                            r"<b>[^<]*(fatal|error|warning|exception)[^<]*</b>:?\s*(?P<result>.+?)<br\s*/?\s*>", 
                             r"<li>Error Type:<br>(?P<result>.+?)</li>", 
                             r"error '[0-9a-f]{8}'((<[^>]+>)|\s)+(?P<result>[^<>]+)"
                         )
