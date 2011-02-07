096efea282acbe045a007014d4b76735456a443d
Miroslav Stampar <miroslav.stampar@gmail.com>
2011-02-07 10:22:43 +0000

    added BULK to EXCLUDE_UNESCAPE and preventing crashes when output=[]

--- lib/takeover/xp_cmdshell.py
+++ lib/takeover/xp_cmdshell.py
@@ -123,10 +123,10 @@ class xp_cmdshell:
         output = inject.getValue("SELECT %s FROM %s" % (self.tblField, self.cmdTblName), resumeValue=False, sort=False, firstChar=first, lastChar=last)
         inject.goStacked("DELETE FROM %s" % self.cmdTblName)
 
-        if isinstance(output, (list, tuple)):
+        if output and isinstance(output, (list, tuple)):
             output = output[0]
 
-            if isinstance(output, (list, tuple)):
+            if output and isinstance(output, (list, tuple)):
                 output = output[0]
 
         return output
