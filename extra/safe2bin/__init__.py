79d58045194cc96b108bd8d3b261074c153cea33
Bernardo Damele <bernardo.damele@gmail.com>
2011-04-15 16:28:48 +0000

    added propset

--- extra/safe2bin/__init__.py
+++ extra/safe2bin/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-$Id: __init__.py 3678 2011-04-15 12:33:18Z stamparm $
+$Id$
 
 Copyright (c) 2006-2011 sqlmap developers (http://sqlmap.sourceforge.net/)
 See the file 'doc/COPYING' for copying permission
