48f916d5a47c271176c8a18dfcbbd9bf2282c5b3
Bernardo Damele <bernardo.damele@gmail.com>
2011-04-15 16:25:42 +0000

    Fixed a minor bug

---
+++ extra/safe2bin/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+$Id: __init__.py 3678 2011-04-15 12:33:18Z stamparm $
+
+Copyright (c) 2006-2011 sqlmap developers (http://sqlmap.sourceforge.net/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+pass
