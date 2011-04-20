44926757da5821beea37365de5074be51f26d7e9
Miroslav Stampar <miroslav.stampar@gmail.com>
2011-04-20 09:23:08 +0000

    minor update

--- lib/takeover/xp_cmdshell.py
+++ lib/takeover/xp_cmdshell.py
@@ -120,7 +120,7 @@ class xp_cmdshell:
 
         self.delRemoteFile(tmpFile)
 
-        output = inject.getValue("SELECT %s FROM %s" % (self.tblField, self.cmdTblName), resumeValue=False, sort=False, firstChar=first, lastChar=last)
+        output = inject.getValue("SELECT %s FROM %s" % (self.tblField, self.cmdTblName), resumeValue=False, sort=False, firstChar=first, lastChar=last, safeCharEncode=False)
         inject.goStacked("DELETE FROM %s" % self.cmdTblName)
 
         if output and isinstance(output, (list, tuple)):
