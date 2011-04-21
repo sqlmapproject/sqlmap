d2f102f5a11062dcc363e76a1c08056f0c72483c
Bernardo Damele <bernardo.damele@gmail.com>
2011-04-21 20:21:37 +0000

    cosmetics

--- lib/request/basic.py
+++ lib/request/basic.py
@@ -143,7 +143,7 @@ def getHeuristicCharEncoding(page):
     """
     retVal = detect(page)['encoding']
 
-    infoMsg  = "heuristics detected web page charset '%s'." % retVal
+    infoMsg  = "heuristics detected web page charset '%s'" % retVal
     singleTimeLogMessage(infoMsg, logging.INFO, retVal)
 
     return retVal
