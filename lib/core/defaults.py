c7e1aeeef2b46ca121b9d31cfe43a4f287a2537c
Bernardo Damele <bernardo.damele@gmail.com>
2011-06-18 11:02:48 +0000

    layout

--- lib/core/defaults.py
+++ lib/core/defaults.py
@@ -10,19 +10,19 @@ See the file 'doc/COPYING' for copying permission
 from lib.core.datatype import advancedDict
 
 _defaults =  {
-                "timeSec":      5,
-                "googlePage":   1,
-                "cpuThrottle":  10,
-                "verbose":      1,
-                "cDel":         ";",
-                "delay":        0,
-                "timeout":      30,
-                "retries":      3,
-                "saFreq":       0,
-                "threads":      1,
-                "level":        1,
-                "risk":         1,
-                "tech":         "BEUST"
-            }
+               "timeSec":      5,
+               "googlePage":   1,
+               "cpuThrottle":  10,
+               "verbose":      1,
+               "cDel":         ";",
+               "delay":        0,
+               "timeout":      30,
+               "retries":      3,
+               "saFreq":       0,
+               "threads":      1,
+               "level":        1,
+               "risk":         1,
+               "tech":         "BEUST"
+             }
 
 defaults = advancedDict(_defaults)
