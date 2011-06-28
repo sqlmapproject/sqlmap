d3b44a5f58dd1ca830d43c6adb84b2a1d60f1c35
Bernardo Damele <bernardo.damele@gmail.com>
2011-06-28 10:59:20 +0000

    Added copyright

--- plugins/dbms/db2/connector.py
+++ plugins/dbms/db2/connector.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+"""
+$Id$
+
+Copyright (c) 2006-2011 sqlmap developers (http://sqlmap.sourceforge.net/)
+See the file 'doc/COPYING' for copying permission
+"""
 
 try:
     import ibm_db_dbi
