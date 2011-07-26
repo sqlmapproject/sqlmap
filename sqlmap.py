ea00c946484ea3d59a22ecad05a8c6d807d52013
Bernardo Damele <bernardo.damele@gmail.com>
2011-07-26 10:10:05 +0000

    Minor bug fix

--- sqlmap.py
+++ sqlmap.py
@@ -130,7 +130,7 @@ def main():
         kb.threadException = True
 
         # Reference: http://stackoverflow.com/questions/1635080/terminate-a-multi-thread-python-program
-        if conf.threads > 1:
+        if hasattr(conf, "threads") and conf.threads > 1:
             os._exit(0)
 
 if __name__ == "__main__":
