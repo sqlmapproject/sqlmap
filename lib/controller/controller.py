a31a0aa8d4ec2e1e7c21a143f383772e2ec1f95a
Miroslav Stampar <miroslav.stampar@gmail.com>
2011-10-06 22:29:49 +0000

    minor update

--- lib/controller/controller.py
+++ lib/controller/controller.py
@@ -244,7 +244,7 @@ def start():
             testSqlInj = False
 
             if PLACE.GET in conf.parameters and not any([conf.data, conf.testParameter]):
-                for parameter in re.findall(r"([^=]+)=([^&]+&?|\Z)", conf.parameters[PLACE.GET]):
+                for parameter in re.findall(r"([^=]+)=([^%s]+%s?|\Z)" % (conf.pDel or ";", conf.pDel or ";"), conf.parameters[PLACE.GET]):
                     paramKey = (conf.hostname, conf.path, PLACE.GET, parameter[0])
 
                     if paramKey not in kb.testedParams:
