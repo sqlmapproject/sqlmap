24d495368e66c5c4732b258d288661dad576c3d7
Miroslav Stampar <miroslav.stampar@gmail.com>
2011-10-25 14:03:11 +0000

    minor update

--- tamper/modsecurityversioned.py
+++ tamper/modsecurityversioned.py
@@ -7,6 +7,7 @@ Copyright (c) 2006-2011 sqlmap developers (http://www.sqlmap.org/)
 See the file 'doc/COPYING' for copying permission
 """
 
+from lib.core.common import randomInt
 from lib.core.enums import PRIORITY
 
 __priority__ = PRIORITY.HIGHER
@@ -42,6 +43,6 @@ def tamper(payload):
                 payload = payload[:payload.find(comment)]
                 break
         if ' ' in payload:
-            retVal = "%s /*!30000%s*/%s" % (payload[:payload.find(' ')], payload[payload.find(' ') + 1:], postfix)
+            retVal = "%s /*!30%s%s*/%s" % (payload[:payload.find(' ')], randomInt(3), payload[payload.find(' ') + 1:], postfix)
 
     return retVal
