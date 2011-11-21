0ce885e6e68f8b484189df94ce2d412fc298725b
Miroslav Stampar <miroslav.stampar@gmail.com>
2011-11-21 12:47:23 +0000

    adding base64encode tampering script

---
+++ tamper/base64encode.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+$Id$
+
+Copyright (c) 2006-2011 sqlmap developers (http://www.sqlmap.org/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+import base64
+
+from lib.core.enums import PRIORITY
+
+__priority__ = PRIORITY.LOWEST
+
+def dependencies():
+    pass
+
+def tamper(payload):
+    """
+    Base64 all characters in a given payload
+
+    Example:
+        * Input: 1' AND SLEEP(5)#
+        * Output: MScgQU5EIFNMRUVQKDUpIw==
+    """
+
+    retVal = payload
+
+    if payload:
+        retVal = base64.b64encode(payload)
+
+    return retVal
