f495cfa139902b06f46993a999a3df27d8bf0dea
Miroslav Stampar <miroslav.stampar@gmail.com>
2012-06-27 23:32:16 +0200

    minor update

--- extra/shutils/blanks.sh
+++ extra/shutils/blanks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# # Copyright (c) 2006-2012 sqlmap developers (http://www.sqlmap.org/)
+# Copyright (c) 2006-2012 sqlmap developers (http://www.sqlmap.org/)
 # See the file 'doc/COPYING' for copying permission
 
 # Removes trailing spaces from blank lines inside project files
--- extra/shutils/duplicates.py
+++ extra/shutils/duplicates.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python
 
-
-"""
-Copyright (c) 2006-2012 sqlmap developers (http://www.sqlmap.org/)
-See the file 'doc/COPYING' for copying permission
+# Copyright (c) 2006-2012 sqlmap developers (http://www.sqlmap.org/)
+# See the file 'doc/COPYING' for copying permission
 
 # Removes duplicate entries in wordlist like files
 
 import sys
 
 if len(sys.argv) > 0:
+    items = list()
 
-items = list()
-f = open(sys.argv[1], 'r')
+    with open(sys.argv[1], 'r') as f:
+        for item in f.readlines():
+            item = item.strip()
+            try:
+                str.encode(item)
+                if item in items:
+                    if item:
+                        print item
+                else:
+                    items.append(item)
+            except:
+                pass
 
-for item in f.readlines():
-item = item.strip()
-try:
-str.encode(item)
-if item in items:
-if item:
-print item
-else:
-items.append(item)
-
-if not item:
-items.append('')
-except:
-pass
-f.close()
-
-f = open(sys.argv[1], 'w+')
-f.writelines("\n".join(items))
+    with open(sys.argv[1], 'w+') as f:
+        f.writelines("\n".join(items))
