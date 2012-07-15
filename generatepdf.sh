2ee0c8fa403b0b2831e13409348670b6d3ad7080
Bernardo Damele <bernardo.damele@gmail.com>
2012-07-15 01:58:41 +0100

    added abstract

--- generatepdf.sh
+++ generatepdf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pandoc -f markdown -o Users-manual.pdf --smart --normalize --tab-stop=4 --standalone --template=template.latex --variable version=1.0 --toc --highlight-style=haddock --number-sections title.txt Users-manual.md
+pandoc -f markdown -o Users-manual.pdf --smart --normalize --tab-stop=4 --standalone --template=template.latex --variable version=1.0 --variable author-meta="sqlmap developers" --variable title-meta="sqlmap user's manual" --toc --highlight-style=haddock --number-sections title.txt Users-manual.md
