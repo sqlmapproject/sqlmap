dbbfee6c93550a9e2e5facfb74227e080f00e04f
Miroslav Stampar <miroslav.stampar@gmail.com>
2012-08-22 15:05:17 +0200

    Minor style update

--- sqlmap.py
+++ sqlmap.py
@@ -10,7 +10,7 @@ import sys
 PYVERSION = sys.version.split()[0]
 
 if PYVERSION >= "3" or PYVERSION < "2.6":
-    exit("[CRITICAL] incompatible Python version detected ('%s'). For successfully running sqlmap you'll have to use version 2.6 or 2.7 (visit \"http://www.python.org/download/\")" % PYVERSION)
+    exit("[CRITICAL] incompatible Python version detected ('%s'). For successfully running sqlmap you'll have to use version 2.6 or 2.7 (visit 'http://www.python.org/download/')" % PYVERSION)
 elif __name__ == "__main__":
     from _sqlmap import main
     from lib.controller.controller import start  # needed for proper working of --profile switch
