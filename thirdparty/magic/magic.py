b0a13be985486d9d829bd116dc985c96e691993f
Miroslav Stampar <miroslav.stampar@gmail.com>
2013-01-18 10:12:45 +0100

    Just a little refactoring

--- thirdparty/magic/magic.py
+++ thirdparty/magic/magic.py
@@ -1,8 +1,6 @@
 """
 magic is a wrapper around the libmagic file identification library.
 
-See README for more information.
-
 Usage:
 
 >>> import magic
@@ -14,7 +12,6 @@ Usage:
 'PDF document, version 1.2'
 >>>
 
-
 """
 
 import sys
@@ -24,12 +21,12 @@ import ctypes.util
 
 from ctypes import c_char_p, c_int, c_size_t, c_void_p
 
-class MagicException(Exception): pass
+class MagicException(Exception):
+    pass
 
 class Magic:
     """
     Magic is a wrapper around the libmagic C library.
-
     """
 
     def __init__(self, mime=False, magic_file=None, mime_encoding=False):
@@ -39,8 +36,8 @@ class Magic:
         mime - if True, mimetypes are returned instead of textual descriptions
         mime_encoding - if True, codec is returned
         magic_file - use a mime database other than the system default
-
         """
+
         flags = MAGIC_NONE
         if mime:
             flags |= MAGIC_MIME
@@ -56,6 +53,7 @@ class Magic:
         """
         Identify the contents of `buf`
         """
+
         return magic_buffer(self.cookie, buf)
 
     def from_file(self, filename):
@@ -103,8 +101,6 @@ def from_file(filename, mime=False):
 def from_buffer(buffer, mime=False):
     m = _get_magic_type(mime)
     return m.from_buffer(buffer)
-
-
 
 try:
     libmagic = None
@@ -178,7 +174,6 @@ try:
     def magic_buffer(cookie, buf):
         return _magic_buffer(cookie, buf, len(buf))
 
-
     _magic_load = libmagic.magic_load
     _magic_load.restype = c_int
     _magic_load.argtypes = [magic_t, c_char_p]
@@ -198,48 +193,28 @@ try:
     magic_compile = libmagic.magic_compile
     magic_compile.restype = c_int
     magic_compile.argtypes = [magic_t, c_char_p]
+
 except ImportError:
     pass
 
-
 MAGIC_NONE = 0x000000 # No flags
-
 MAGIC_DEBUG = 0x000001 # Turn on debugging
-
 MAGIC_SYMLINK = 0x000002 # Follow symlinks
-
 MAGIC_COMPRESS = 0x000004 # Check inside compressed files
-
 MAGIC_DEVICES = 0x000008 # Look at the contents of devices
-
 MAGIC_MIME = 0x000010 # Return a mime string
-
 MAGIC_MIME_ENCODING = 0x000400 # Return the MIME encoding
-
 MAGIC_CONTINUE = 0x000020 # Return all matches
-
 MAGIC_CHECK = 0x000040 # Print warnings to stderr
-
 MAGIC_PRESERVE_ATIME = 0x000080 # Restore access time on exit
-
 MAGIC_RAW = 0x000100 # Don't translate unprintable chars
-
 MAGIC_ERROR = 0x000200 # Handle ENOENT etc as real errors
-
 MAGIC_NO_CHECK_COMPRESS = 0x001000 # Don't check for compressed files
-
 MAGIC_NO_CHECK_TAR = 0x002000 # Don't check for tar files
-
 MAGIC_NO_CHECK_SOFT = 0x004000 # Don't check magic entries
-
 MAGIC_NO_CHECK_APPTYPE = 0x008000 # Don't check application type
-
 MAGIC_NO_CHECK_ELF = 0x010000 # Don't check for elf details
-
 MAGIC_NO_CHECK_ASCII = 0x020000 # Don't check for ascii files
-
 MAGIC_NO_CHECK_TROFF = 0x040000 # Don't check ascii/troff
-
 MAGIC_NO_CHECK_FORTRAN = 0x080000 # Don't check ascii/fortran
-
 MAGIC_NO_CHECK_TOKENS = 0x100000 # Don't check ascii/tokens
