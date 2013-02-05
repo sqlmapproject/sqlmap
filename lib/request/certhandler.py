62772125e365c89f5c0c9e322f798d8e3c5dbed2
Miroslav Stampar <miroslav.stampar@gmail.com>
2013-02-05 12:16:06 +0100

    Bug fix for HTTPSCertAuthHandler

--- lib/request/certhandler.py
+++ lib/request/certhandler.py
@@ -7,7 +7,6 @@ See the file 'doc/COPYING' for copying permission
 
 import httplib
 import urllib2
-import sys
 
 from lib.core.data import conf
 
@@ -20,9 +19,5 @@ class HTTPSCertAuthHandler(urllib2.HTTPSHandler):
     def https_open(self, req):
         return self.do_open(self.getConnection, req)
 
-    def getConnection(self, host):
-        if sys.version_info >= (2, 6):
-            retVal = httplib.HTTPSConnection(host, key_file=self.key_file, cert_file=self.cert_file, timeout=conf.timeout)
-        else:
-            retVal = httplib.HTTPSConnection(host, key_file=self.key_file, cert_file=self.cert_file)
-        return retVal
+    def getConnection(self, host, timeout=None):
+        return httplib.HTTPSConnection(host, key_file=self.key_file, cert_file=self.cert_file, timeout=conf.timeout)
