060eac110a7046c11695974cce79a6888fa582b1
Miroslav Stampar <miroslav.stampar@gmail.com>
2013-02-06 10:28:17 +0100

    Cleaner version checking

---
+++ lib/utils/versioncheck.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2013 sqlmap developers (http://sqlmap.org/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+import sys
+
+PYVERSION = sys.version.split()[0]
+
+if PYVERSION >= "3" or PYVERSION < "2.6":
+    exit("[CRITICAL] incompatible Python version detected ('%s'). For successfully running sqlmap you'll have to use version 2.6 or 2.7 (visit 'http://www.python.org/download/')" % PYVERSION)
