5857a09e2ed5dc6cb132ffe67516f0647d5d77bf
stamparm <miroslav.stampar@gmail.com>
2013-03-20 10:32:15 +0100

    New WAF scripts

---
+++ waf/jiasule.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2013 sqlmap developers (http://sqlmap.org/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+import re
+
+from lib.core.enums import HTTPHEADER
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Jiasule Web Application Firewall (Jiasule)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, headers, code = get_page(get=vector)
+        retval = re.search(r"jiasule-WAF", headers.get(HTTPHEADER.SERVER, ""), re.I) is not None
+        retval |= re.search(r"static\.jiasule\.com/static/js/http_error\.js", page, re.I) is not None
+        if retval:
+            break
+
+    return retval
