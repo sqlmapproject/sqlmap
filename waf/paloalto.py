86b62dc6191093ec6b0595323c7b82f2de4bba7d
stamparm <miroslav.stampar@gmail.com>
2013-07-18 16:17:28 +0200

    Adding a new WAF script

---
+++ waf/paloalto.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2013 sqlmap developers (http://sqlmap.org/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+import re
+
+from lib.core.enums import HTTP_HEADER
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Palo Alto Firewall (Palo Alto Networks)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, headers, code = get_page(get=vector)
+        retval = re.search(r"Access[^<]+has been blocked in accordance with company policy", page, re.I) is not None
+        if retval:
+            break
+
+    return retval
