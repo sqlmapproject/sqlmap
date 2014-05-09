a72d73804ed388710edba09df6be499269874f97
Miroslav Stampar <miroslav.stampar@gmail.com>
2014-05-10 01:31:44 +0200

    Revert of 925517489041b39b2d22ea3ce2e5a7661d4f4c26 (bug was introduced with it)

--- lib/core/common.py
+++ lib/core/common.py
@@ -1992,7 +1992,7 @@ def getUnicode(value, encoding=None, system=False, noneToNull=False):
                 try:
                     return unicode(value, encoding or kb.get("pageEncoding") or UNICODE_ENCODING)
                 except UnicodeDecodeError, ex:
-                    return value[:ex.start] + "".join(INVALID_UNICODE_CHAR_FORMAT % ord(_) for _ in value[ex.start:ex.end]) + value[ex.end:]
+                    value = value[:ex.start] + "".join(INVALID_UNICODE_CHAR_FORMAT % ord(_) for _ in value[ex.start:ex.end]) + value[ex.end:]
         else:
             try:
                 return unicode(value)
