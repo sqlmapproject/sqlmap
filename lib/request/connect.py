20c272b77d12f3c3c6b804369689efe1b82976d3
Miroslav Stampar <miroslav.stampar@gmail.com>
2014-12-07 16:14:48 +0100

    More generic patch for an Issue #994

--- lib/request/connect.py
+++ lib/request/connect.py
@@ -543,7 +543,7 @@ class Connect(object):
                 warnMsg = "connection timed out to the target URL"
             elif "URLError" in tbMsg or "error" in tbMsg:
                 warnMsg = "unable to connect to the target URL"
-            elif "unpack requires a string argument of length 4" in tbMsg:
+            elif "NTLM" in tbMsg:
                 warnMsg = "there has been a problem with NTLM authentication"
             elif "BadStatusLine" in tbMsg:
                 warnMsg = "connection dropped or unknown HTTP "
