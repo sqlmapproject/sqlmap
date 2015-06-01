87b5262ef7e8e287f5b71894222ccf9c7722f3ee
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-06-01 14:18:21 +0200

    Minor patch

--- tamper/symboliclogical.py
+++ tamper/symboliclogical.py
@@ -25,6 +25,6 @@ def tamper(payload, **kwargs):
     retVal = payload
 
     if payload:
-        retVal = re.sub(r"(?i)\bAND\b", "&&", re.sub(r"(?i)\bOR\b", "||", payload))
+        retVal = re.sub(r"(?i)\bAND\b", "%26%26", re.sub(r"(?i)\bOR\b", "%7C%7C", payload))
 
     return retVal
