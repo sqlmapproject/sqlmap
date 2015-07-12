ffd949882711d5885438bb6a7d48bf242bbcf229
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-07-12 12:24:06 +0200

    Bug fix

--- waf/sucuri.py
+++ waf/sucuri.py
@@ -13,12 +13,12 @@ from lib.core.settings import WAF_ATTACK_VECTORS
 __product__ = "Sucuri WebSite Firewall"
 
 def detect(get_page):
-    retval = False
+    retVal = False
 
     for vector in WAF_ATTACK_VECTORS:
         page, headers, code = get_page(get=vector)
         retVal = code == 403 and re.search(r"Sucuri/Cloudproxy", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
-        if retval:
+        if retVal:
             break
 
-    return retval
+    return retVal
