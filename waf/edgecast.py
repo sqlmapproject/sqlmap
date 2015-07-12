4800ea7311eee7350de7620f8afe8728e0a9b126
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-07-12 12:17:26 +0200

    Bug fix

--- waf/edgecast.py
+++ waf/edgecast.py
@@ -13,12 +13,12 @@ from lib.core.settings import WAF_ATTACK_VECTORS
 __product__ = "EdgeCast WAF (Verizon)"
 
 def detect(get_page):
-    retval = False
+    retVal = False
 
     for vector in WAF_ATTACK_VECTORS:
         page, headers, code = get_page(get=vector)
         retVal = code == 400 and re.search(r"\AECDF", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
-        if retval:
+        if retVal:
             break
 
-    return retval
+    return retVal
