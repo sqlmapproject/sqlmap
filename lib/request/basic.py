c31e23e514ccbc5a727c3453eebc27d729d914f1
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-11-06 11:19:55 +0100

    Patch for an Issue #1516

--- lib/request/basic.py
+++ lib/request/basic.py
@@ -114,7 +114,7 @@ def forgeHeaders(items=None):
                 elif not kb.testMode:
                     headers[HTTP_HEADER.COOKIE] += "%s %s=%s" % (conf.cookieDel or DEFAULT_COOKIE_DELIMITER, cookie.name, getUnicode(cookie.value))
 
-        if kb.testMode and not conf.csrfToken:
+        if kb.testMode and not any((conf.csrfToken, conf.safeUrl)):
             resetCookieJar(conf.cj)
 
     return headers
