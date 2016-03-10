e1ce16144afaf3a6c5ef9d5162f672d8a88d59f4
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-03-10 15:42:01 +0100

    Fixes #1753

--- lib/core/settings.py
+++ lib/core/settings.py
@@ -20,7 +20,7 @@ from lib.core.enums import OS
 from lib.core.revision import getRevisionNumber
 
 # sqlmap version and site
-VERSION = "1.0.0.16"
+VERSION = "1.0.0.17"
 REVISION = getRevisionNumber()
 STABLE = VERSION.count('.') <= 2
 VERSION_STRING = "sqlmap/%s#%s" % (VERSION, "stable" if STABLE else "dev")
