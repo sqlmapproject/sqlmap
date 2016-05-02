6db3bcbb519436e1ac67bdf17b25e6b5bd3f9018
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-05-02 10:12:19 +0200

    Minor update for UrlScan

--- waf/urlscan.py
+++ waf/urlscan.py
@@ -16,8 +16,9 @@ def detect(get_page):
     retval = False
 
     for vector in WAF_ATTACK_VECTORS:
-        _, headers, _ = get_page(get=vector)
+        page, headers, code = get_page(get=vector)
         retval = re.search(r"Rejected-By-UrlScan", headers.get(HTTP_HEADER.LOCATION, ""), re.I) is not None
+        retval |= code != 200 and re.search(r"/Rejected-By-UrlScan", page or "", re.I) is not None
         if retval:
             break
 
