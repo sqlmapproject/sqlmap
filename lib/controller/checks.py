32ee586e2a2a509fa55951047134644f26981683
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-05-22 14:30:32 +0200

    Minor language update

--- lib/controller/checks.py
+++ lib/controller/checks.py
@@ -694,7 +694,7 @@ def checkSqlInjection(place, parameter, value):
     # Return the injection object
     if injection.place is not None and injection.parameter is not None:
         if not conf.dropSetCookie and PAYLOAD.TECHNIQUE.BOOLEAN in injection.data and injection.data[PAYLOAD.TECHNIQUE.BOOLEAN].vector.startswith('OR'):
-            warnMsg = "in OR boolean-based injections, please consider usage "
+            warnMsg = "in OR boolean-based injection cases, please consider usage "
             warnMsg += "of switch '--drop-set-cookie' if you experience any "
             warnMsg += "problems during data retrieval"
             logger.warn(warnMsg)
