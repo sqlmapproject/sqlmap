e36fc0228278fa028f1490f564c8c40c56546b14
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-05-27 10:17:42 +0200

    Adding sophos WAF script

---
+++ waf/sophos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2016 sqlmap developers (http://sqlmap.org/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "UTM Web Protection (Sophos)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, _, _ = get_page(get=vector)
+        retval = "Powered by UTM Web Protection" in (page or "")
+        if retval:
+            break
+
+    return retval
