4cd9fdb7df60d76ad0113e286265d99f71a83c55
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-05-27 09:27:45 +0200

    Minor update for F5 waf script

--- waf/bigip.py
+++ waf/bigip.py
@@ -20,6 +20,7 @@ def detect(get_page):
         retval = headers.get("X-Cnection", "").lower() == "close"
         retval |= re.search(r"\ATS[a-zA-Z0-9]{3,6}=", headers.get(HTTP_HEADER.SET_COOKIE, ""), re.I) is not None
         retval |= re.search(r"BigIP|BIGipServer", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
+        retval |= re.search(r"\AF5\Z", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
         if retval:
             break
 
