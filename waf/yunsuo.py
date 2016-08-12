a45a90df9409df5529de67d5d04606370f6ba52a
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-08-12 14:32:03 +0200

    Adding new WAF script (Yunsuo)

---
+++ waf/yunsuo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2016 sqlmap developers (http://sqlmap.org/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+import re
+
+from lib.core.enums import HTTP_HEADER
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Yunsuo Web Application Firewall (Yunsuo)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, headers, _ = get_page(get=vector)
+        retval = re.search(r"<img class=\"yunsuologo\"", page, re.I) is not None
+        retval |= re.search(r"yunsuo_session", headers.get(HTTP_HEADER.SET_COOKIE, ""), re.I) is not None
+        if retval:
+            break
+
+    return retval
