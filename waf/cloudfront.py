63a74777f2c2f313f7a09b2ebfc1f389925c319d
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-12-01 23:08:49 +0100

    Minor update

--- waf/cloudfront.py
+++ waf/cloudfront.py
@@ -7,6 +7,7 @@ See the file 'doc/COPYING' for copying permission
 
 import re
 
+from lib.core.enums import HTTP_HEADER
 from lib.core.settings import WAF_ATTACK_VECTORS
 
 __product__ = "CloudFront (Amazon)"
@@ -17,6 +18,7 @@ def detect(get_page):
     for vector in WAF_ATTACK_VECTORS:
         _, headers, _ = get_page(get=vector)
 
+        retval |= re.search(r"cloudfront", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
         retval |= re.search(r"cloudfront", headers.get("X-Cache", ""), re.I) is not None
         retval |= headers.get("X-Amz-Cf-Id") is not None
 
