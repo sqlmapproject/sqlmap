4bd7d81cea58b89165772f1b24574d3d5390e1a6
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-12-09 23:14:18 +0100

    Patches #2300

--- plugins/dbms/informix/enumeration.py
+++ plugins/dbms/informix/enumeration.py
@@ -10,3 +10,25 @@ from plugins.generic.enumeration import Enumeration as GenericEnumeration
 class Enumeration(GenericEnumeration):
     def __init__(self):
         GenericEnumeration.__init__(self)
+
+    def searchDb(self):
+        warnMsg = "on Informix searching of databases is not implemented"
+        logger.warn(warnMsg)
+
+        return []
+
+    def searchTable(self):
+        warnMsg = "on Informix searching of tables is not implemented"
+        logger.warn(warnMsg)
+
+        return []
+
+    def searchColumn(self):
+        warnMsg = "on Informix searching of columns is not implemented"
+        logger.warn(warnMsg)
+
+        return []
+
+    def search(self):
+        warnMsg = "on Informix search option is not available"
+        logger.warn(warnMsg)
