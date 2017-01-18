cf182882b129864829be3de04c970209d12fb79b
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-01-18 10:40:39 +0100

    Minor update

--- waf/tencent.py
+++ waf/tencent.py
@@ -9,13 +9,13 @@ import re
 
 from lib.core.settings import WAF_ATTACK_VECTORS
 
-__product__ = "TencentCloud Web Application Firewall (TencentCloud)"
+__product__ = "Tencent Cloud Web Application Firewall (Tencent Cloud Computing)"
 
 def detect(get_page):
     retval = False
 
     for vector in WAF_ATTACK_VECTORS:
-        page, headers, code = get_page(get=vector)
+        page, _, code = get_page(get=vector)
         retval = code == 405 and "waf.tencent-cloud.com" in (page or "")
         if retval:
             break
