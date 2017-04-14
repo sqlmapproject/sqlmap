a702dafd035db0add45d096d9a8860d5a5b21eb7
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-04-14 12:47:24 +0200

    Fixes #2481

--- lib/request/connect.py
+++ lib/request/connect.py
@@ -310,8 +310,8 @@ class Connect(object):
 
             elif target:
                 if conf.forceSSL and urlparse.urlparse(url).scheme != "https":
-                    url = re.sub("\Ahttp:", "https:", url, re.I)
-                    url = re.sub(":80/", ":443/", url, re.I)
+                    url = re.sub("(?i)\Ahttp:", "https:", url)
+                    url = re.sub("(?i):80/", ":443/", url)
 
                 if PLACE.GET in conf.parameters and not get:
                     get = conf.parameters[PLACE.GET]
