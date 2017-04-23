15a1d55812fdf53f943a06275ab62be96ecfa33d
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-04-23 23:14:05 +0200

    Fixes #2500

--- lib/core/option.py
+++ lib/core/option.py
@@ -1684,10 +1684,10 @@ def _cleanupOptions():
         setOptimize()
 
     if conf.data:
-        conf.data = re.sub(INJECT_HERE_MARK.replace(" ", r"[^A-Za-z]*"), CUSTOM_INJECTION_MARK_CHAR, conf.data, re.I)
+        conf.data = re.sub(INJECT_HERE_MARK.replace(" ", r"[^A-Za-z]*"), CUSTOM_INJECTION_MARK_CHAR, conf.data, flags=re.I)
 
     if conf.url:
-        conf.url = re.sub(INJECT_HERE_MARK.replace(" ", r"[^A-Za-z]*"), CUSTOM_INJECTION_MARK_CHAR, conf.url, re.I)
+        conf.url = re.sub(INJECT_HERE_MARK.replace(" ", r"[^A-Za-z]*"), CUSTOM_INJECTION_MARK_CHAR, conf.url, flags=re.I)
 
     if conf.os:
         conf.os = conf.os.capitalize()
