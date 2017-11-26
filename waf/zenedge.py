792ed52ab5eebf53db2e04ad4890bd6c6f6fe6bc
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-11-26 12:06:23 +0100

    New WAF scripts

---
+++ waf/zenedge.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2017 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from lib.core.enums import HTTP_HEADER
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Zenedge Web Application Firewall (Zenedge)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        _, headers, code = get_page(get=vector)
+        retval = code >= 400 and re.search(r"\AZENEDGE", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
+        if retval:
+            break
+
+    return retval
