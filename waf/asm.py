2d4391dc361ffba8121bc11e3bcad50767e1a5e4
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-12-13 15:10:15 +0100

    Adding new WAF script (F5 ASM)

---
+++ waf/asm.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2017 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Application Security Manager (F5 Networks)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, _, _ = get_page(get=vector)
+        retval = "The requested URL was rejected. Please consult with your administrator." in (page or "")
+        if retval:
+            break
+
+    return retval
