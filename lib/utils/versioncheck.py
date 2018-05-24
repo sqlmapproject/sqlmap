d2d829abf5b2033c8db94a69cc1e8d3046de8826
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-05-24 10:07:35 +0200

    Patch related to the #3117

--- lib/utils/versioncheck.py
+++ lib/utils/versioncheck.py
@@ -12,7 +12,7 @@ PYVERSION = sys.version.split()[0]
 if PYVERSION >= "3" or PYVERSION < "2.6":
     exit("[CRITICAL] incompatible Python version detected ('%s'). To successfully run sqlmap you'll have to use version 2.6.x or 2.7.x (visit 'https://www.python.org/downloads/')" % PYVERSION)
 
-extensions = ("bz2", "gzip", "ssl", "sqlite3", "zlib")
+extensions = ("bz2", "gzip", "pyexpat", "ssl", "sqlite3", "zlib")
 try:
     for _ in extensions:
         __import__(_)
