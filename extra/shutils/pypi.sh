2542b6d2414cdc35a5ce53e774d00956bd815b57
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-08-20 19:44:25 +0200

    Minor patch (https version of site is not available)

--- extra/shutils/pypi.sh
+++ extra/shutils/pypi.sh
@@ -29,7 +29,7 @@ setup(
     long_description='sqlmap is an open source penetration testing tool that automates the process of detecting and exploiting SQL injection flaws and taking over of database servers. It comes with a powerful detection engine, many niche features for the ultimate penetration tester and a broad range of switches lasting from database fingerprinting, over data fetching from the database, to accessing the underlying file system and executing commands on the operating system via out-of-band connections.',
     author='Bernardo Damele Assumpcao Guimaraes, Miroslav Stampar',
     author_email='bernardo@sqlmap.org, miroslav@sqlmap.org',
-    url='https://sqlmap.org',
+    url='http://sqlmap.org',
     download_url='https://github.com/sqlmapproject/sqlmap/archive/$VERSION.zip',
     license='GNU General Public License v2 (GPLv2)',
     packages=find_packages(),
