bc13d8923b5d2758dc3335d8d2b1212c6142dd27
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-08-20 19:43:09 +0200

    Adding long_description to PyPI upload script

--- extra/shutils/pypi.sh
+++ extra/shutils/pypi.sh
@@ -25,7 +25,8 @@ from setuptools import setup, find_packages
 setup(
     name='sqlmap',
     version='$VERSION',
-    description="Automatic SQL injection and database takeover tool",
+    description='Automatic SQL injection and database takeover tool',
+    long_description='sqlmap is an open source penetration testing tool that automates the process of detecting and exploiting SQL injection flaws and taking over of database servers. It comes with a powerful detection engine, many niche features for the ultimate penetration tester and a broad range of switches lasting from database fingerprinting, over data fetching from the database, to accessing the underlying file system and executing commands on the operating system via out-of-band connections.',
     author='Bernardo Damele Assumpcao Guimaraes, Miroslav Stampar',
     author_email='bernardo@sqlmap.org, miroslav@sqlmap.org',
     url='https://sqlmap.org',
