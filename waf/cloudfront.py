dc01f2e77358d801d076c6cddf32670fdbc3ab8d
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-08-30 16:42:35 +0200

    Fine tuning of Cloudfrount WAF script (less FP)

--- waf/cloudfront.py
+++ waf/cloudfront.py
@@ -18,8 +18,7 @@ def detect(get_page):
     for vector in WAF_ATTACK_VECTORS:
         _, headers, _ = get_page(get=vector)
 
-        retval |= re.search(r"cloudfront", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
-        retval |= re.search(r"cloudfront", headers.get("X-Cache", ""), re.I) is not None
+        retval = re.search(r"Error from cloudfront", headers.get("X-Cache", ""), re.I) is not None
         retval |= headers.get("X-Amz-Cf-Id") is not None
 
         if retval:
