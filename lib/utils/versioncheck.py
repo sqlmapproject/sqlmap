63b84c31e5e55fa1005ce4f524a3b8f3c4a42987
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-08 23:36:08 +0200

    Update regarding the #3229

--- lib/utils/versioncheck.py
+++ lib/utils/versioncheck.py
@@ -12,12 +12,16 @@ PYVERSION = sys.version.split()[0]
 if PYVERSION >= "3" or PYVERSION < "2.6":
     exit("[CRITICAL] incompatible Python version detected ('%s'). To successfully run sqlmap you'll have to use version 2.6.x or 2.7.x (visit 'https://www.python.org/downloads/')" % PYVERSION)
 
+errors = []
 extensions = ("bz2", "gzip", "pyexpat", "ssl", "sqlite3", "zlib")
-try:
-    for _ in extensions:
+for _ in extensions:
+    try:
         __import__(_)
-except ImportError:
-    errMsg = "missing one or more core extensions (%s) " % (", ".join("'%s'" % _ for _ in extensions))
+    except ImportError:
+        errors.append(_)
+
+if errors:
+    errMsg = "missing one or more core extensions (%s) " % (", ".join("'%s'" % _ for _ in errors))
     errMsg += "most likely because current version of Python has been "
-    errMsg += "built without appropriate dev packages (e.g. 'libsqlite3-dev')"
+    errMsg += "built without appropriate dev packages"
     exit(errMsg)
