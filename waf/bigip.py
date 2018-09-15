2d2b20344d57be1a1e6aa9ea2d60542d731429db
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-15 23:17:32 +0200

    Minor patch for bigip WAF script

--- waf/bigip.py
+++ waf/bigip.py
@@ -19,7 +19,7 @@ def detect(get_page):
         _, headers, code = get_page(get=vector)
         retval = headers.get("X-Cnection", "").lower() == "close"
         retval |= headers.get("X-WA-Info") is not None
-        retval |= re.search(r"\ATS\w{4,}=", headers.get(HTTP_HEADER.SET_COOKIE, ""), re.I) is not None
+        retval |= re.search(r"\bTS[0-9a-f]+=", headers.get(HTTP_HEADER.SET_COOKIE, "")) is not None
         retval |= re.search(r"BigIP|BIGipServer", headers.get(HTTP_HEADER.SET_COOKIE, ""), re.I) is not None
         retval |= re.search(r"BigIP|BIGipServer", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
         retval |= re.search(r"\AF5\Z", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
