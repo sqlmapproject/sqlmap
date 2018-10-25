c63d1e79efb91ed0d482bb9e8665d1571299e796
Jennifer Torres <jtorresf42@gmail.com>
2018-10-25 17:44:08 +0100

    Update luanginxwafbypass.py
    Update header.

--- tamper/luanginxwafbypass.py
+++ tamper/luanginxwafbypass.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Copyright (c) 2006-2018 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
 '''
 [+] LUA-Nginx WAFs Bypass (Cloudflare)
 Vulnerability discovered by: Daniel Fariña Hernández
