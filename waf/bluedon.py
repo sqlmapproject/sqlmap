4c9e0b9f1e1a364477961a42542454f77c1560e9
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-12-18 00:35:08 +0100

    Adding Bluedon WAF script

---
+++ waf/bluedon.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2018 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from lib.core.enums import HTTP_HEADER
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Bluedon Web Application Firewall (Bluedon Information Security Technology)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, headers, code = get_page(get=vector)
+        retval = re.search(r"BDWAF", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
+        retval |= re.search(r"Bluedon Web Application Firewall", page or "", re.I) is not None
+        if retval:
+            break
+
+    return retval
