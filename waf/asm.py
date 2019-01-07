8ceff3dcc7539e4058d4720bae8c4d56fb3b8b90
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-07 14:13:29 +0100

    Cleaning junk and updating asm.py WAF script

--- waf/asm.py
+++ waf/asm.py
@@ -5,6 +5,8 @@ Copyright (c) 2006-2019 sqlmap developers (http://sqlmap.org/)
 See the file 'LICENSE' for copying permission
 """
 
+import re
+
 from lib.core.settings import WAF_ATTACK_VECTORS
 
 __product__ = "Application Security Manager (F5 Networks)"
@@ -13,9 +15,10 @@ def detect(get_page):
     retval = False
 
     for vector in WAF_ATTACK_VECTORS:
-        page, _, _ = get_page(get=vector)
+        page, headers, code = get_page(get=vector)
         retval = "The requested URL was rejected. Please consult with your administrator." in (page or "")
         retval |= all(_ in (page or "") for _ in ("This page can't be displayed. Contact support for additional information", "The incident ID is:"))
+        retval |= (code >= 400) and "ID" in (page or "") and re.search(r"\b\d{19}\b", page or "") is not None
         if retval:
             break
 
