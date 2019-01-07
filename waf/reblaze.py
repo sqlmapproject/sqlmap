12883cac166701434354d3aed7c7de6f1f8ca8c6
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-07 13:45:01 +0100

    Minor update

--- waf/reblaze.py
+++ waf/reblaze.py
@@ -16,9 +16,10 @@ def detect(get_page):
     retval = False
 
     for vector in WAF_ATTACK_VECTORS:
-        _, headers, _ = get_page(get=vector)
+        page, headers, _ = get_page(get=vector)
         retval = re.search(r"\Arbzid=", headers.get(HTTP_HEADER.SET_COOKIE, ""), re.I) is not None
         retval |= re.search(r"Reblaze Secure Web Gateway", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
+        retval |= all(_ in (page or "") for _ in ("Current session has been terminated", "For further information, do not hesitate to contact us", "Access denied (403)"))
         if retval:
             break
 
