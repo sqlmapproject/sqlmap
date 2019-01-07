bf207a7ea667a3e74e124368857199268d50bba3
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-07 16:06:33 +0100

    Minor improvement

--- waf/asm.py
+++ waf/asm.py
@@ -19,6 +19,7 @@ def detect(get_page):
         retval = "The requested URL was rejected. Please consult with your administrator." in (page or "")
         retval |= all(_ in (page or "") for _ in ("This page can't be displayed. Contact support for additional information", "The incident ID is:"))
         retval |= re.search(r"(?i)Support.ID", page or "") is not None and re.search(r"\b\d{19}\b", page or "") is not None
+        retval |= all(_ in (page or "") for _ in ("security.f5aas.com", "Please enable JavaScript to view the page content"))
         if retval:
             break
 
