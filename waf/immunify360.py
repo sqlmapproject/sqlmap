54d0678cbe2f6d347fc970548c4a482c89631b1e
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-07 01:21:07 +0100

    Adding new WAF scripts

---
+++ waf/immunify360.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2019 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from lib.core.enums import HTTP_HEADER
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Imunify360 (CloudLinux Inc.)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, headers, _ = get_page(get=vector)
+        retval = re.search(r"\Aimunify360", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
+        retval = any(_ in (page or "") for _ in ("protected by Imunify360", "Powered by Imunify360", "imunify360 preloader"))
+        if retval:
+            break
+
+    return retval
---
+++ waf/onmessage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2019 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "onMessage Shield (Blackbaud)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, headers, _ = get_page(get=vector)
+        retval = re.search(r"onMessage Shield", headers.get("X-Engine", ""), re.I) is not None
+        retval |= "This site is protected by an enhanced security system to ensure a safe browsing experience" in (page or "")
+        retval |= "onMessage SHIELD" in (page or "")
+        if retval:
+            break
+
+    return retval
