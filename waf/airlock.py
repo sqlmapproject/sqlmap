aa7af33fd54f4bcebf9eedecaa27e7a6f31086db
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-10 13:40:51 +0100

    Update of airlock WAF script

--- waf/airlock.py
+++ waf/airlock.py
@@ -16,8 +16,9 @@ def detect(get_page):
     retval = False
 
     for vector in WAF_ATTACK_VECTORS:
-        _, headers, _ = get_page(get=vector)
+        page, headers, _ = get_page(get=vector)
         retval = re.search(r"\AAL[_-]?(SESS|LB)", headers.get(HTTP_HEADER.SET_COOKIE, ""), re.I) is not None
+        retval |= all(_ in (page or "") for _ in ("The server detected a syntax error in your request", "Check your request and all parameters", "Bad Request", "Your request ID was"))
         if retval:
             break
 
