17be4d63741d5a0b8576d2a1e60a13d0e10430ed
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-10 16:05:46 +0100

    Minor update

--- waf/secureentry.py
+++ waf/secureentry.py
@@ -10,14 +10,14 @@ import re
 from lib.core.enums import HTTP_HEADER
 from lib.core.settings import WAF_ATTACK_VECTORS
 
-__product__ = "USP Secure Entry Server (United Security Providers)"
+__product__ = "Secure Entry Server (United Security Providers)"
 
 def detect(get_page):
     retval = False
 
     for vector in WAF_ATTACK_VECTORS:
-        _, headers, _ = get_page(get=vector)
-        retval = re.search(r"Secure Entry Server", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
+        page, headers, code = get_page(get=vector)
+        retval = code >= 400 and re.search(r"Secure Entry Server", headers.get(HTTP_HEADER.SERVER, ""), re.I) is not None
         if retval:
             break
 
