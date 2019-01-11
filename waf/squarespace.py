0a3144ebb5e2d70ada7be47f939771a43f0ee20e
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-12 00:15:13 +0100

    New WAF script (Squarespace)

---
+++ waf/squarespace.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2019 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Squarespace Web Application Firewall (Squarespace)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, _, _ = get_page(get=vector)
+        retval = all(_ in (page or "") for _ in ("BRICK-50", " @ ", "404 Not Found"))
+        if retval:
+            break
+
+    return retval
