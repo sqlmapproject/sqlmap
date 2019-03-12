eb62397c92f151196c4ac0e34eea4e3952b8523a
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-03-12 14:12:23 +0100

    Minor update of Oracle fingerprinting

--- plugins/dbms/oracle/fingerprint.py
+++ plugins/dbms/oracle/fingerprint.py
@@ -103,7 +103,7 @@ class Fingerprint(GenericFingerprint):
             logger.info(infoMsg)
 
             # Reference: https://en.wikipedia.org/wiki/Oracle_Database
-            for version in ("12c", "11g", "10g", "9i", "8i"):
+            for version in ("18c", "12c", "11g", "10g", "9i", "8i"):
                 number = int(re.search(r"([\d]+)", version).group(1))
                 output = inject.checkBooleanExpression("%d=(SELECT SUBSTR((VERSION),1,%d) FROM SYS.PRODUCT_COMPONENT_VERSION WHERE ROWNUM=1)" % (number, 1 if number < 10 else 2))
 
