89c6cc725bee6c1beab3751ce9d83e6425002d24
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-04-08 22:56:31 +0200

    Probably fixes #3570

--- waf/generic.py
+++ waf/generic.py
@@ -7,6 +7,7 @@ See the file 'LICENSE' for copying permission
 
 import re
 
+from lib.core.common import getUnicode
 from lib.core.data import kb
 from lib.core.settings import GENERIC_PROTECTION_REGEX
 from lib.core.settings import IPS_WAF_CHECK_PAYLOAD
@@ -26,7 +27,7 @@ def detect(get_page):
 
         if code >= 400 or (IPS_WAF_CHECK_PAYLOAD in vector and (code is None or re.search(GENERIC_PROTECTION_REGEX, page or "") and not re.search(GENERIC_PROTECTION_REGEX, original or ""))):
             if code is not None:
-                kb.wafSpecificResponse = "HTTP/1.1 %s\n%s\n%s" % (code, "".join(_ for _ in (headers.headers if headers else {}) or [] if not _.startswith("URI")), page)
+                kb.wafSpecificResponse = "HTTP/1.1 %s\n%s\n%s" % (code, "".join(getUnicode(_) for _ in (headers.headers if headers else {}) or [] if not _.startswith("URI")), getUnicode(page or ""))
 
             retval = True
             break
