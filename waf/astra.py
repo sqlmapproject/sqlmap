9a0a80302573865c6ebf4495d4a0f31e0bf03fea
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-04-23 00:31:20 +0200

    Adding new WAF script (based on identYwaf update)

---
+++ waf/astra.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+
+"""
+Copyright (c) 2006-2019 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+from lib.core.settings import WAF_ATTACK_VECTORS
+
+__product__ = "Astra (Czar Securities)"
+
+def detect(get_page):
+    retval = False
+
+    for vector in WAF_ATTACK_VECTORS:
+        page, headers, code = get_page(get=vector)
+        retval |= all(_ in (page or "") for _ in ("unfortunately our website protection system", "//www.getastra.com"))
+        if retval:
+            break
+
+    return retval
