6bbfec91b42f8fa9f5316e60c8dcfdcd6e101aa9
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-05-24 23:51:58 +0200

    Minor update

--- thirdparty/ansistrm/ansistrm.py
+++ thirdparty/ansistrm/ansistrm.py
@@ -187,9 +187,14 @@ class ColorizingStreamHandler(logging.StreamHandler):
                                 string = match.group(1)
                                 message = message.replace("'%s'" % string, "'%s'" % ''.join((self.csi, str(self.color_map["white"] + 30), 'm', string, self._reset(message))), 1)
                         else:
-                            for match in re.finditer(r"[^\w]'([^']+)'", message):  # single-quoted
+                            match = re.search(r" \('(.+)'\)\Z", message)
+                            if match:
                                 string = match.group(1)
                                 message = message.replace("'%s'" % string, "'%s'" % ''.join((self.csi, str(self.color_map["white"] + 30), 'm', string, self._reset(message))), 1)
+                            else:
+                                for match in re.finditer(r"[^\w]'([^']+)'", message):  # single-quoted
+                                    string = match.group(1)
+                                    message = message.replace("'%s'" % string, "'%s'" % ''.join((self.csi, str(self.color_map["white"] + 30), 'm', string, self._reset(message))), 1)
                 else:
                     message = ''.join((self.csi, ';'.join(params), 'm', message, self.reset))
 
