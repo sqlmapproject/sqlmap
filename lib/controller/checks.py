f9fe1dde73f43fd2e8facb9415df2758b48f1d8d
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-06-01 12:55:53 +0200

    Minor patch (WAFs with 404)

--- lib/controller/checks.py
+++ lib/controller/checks.py
@@ -1384,7 +1384,7 @@ def checkWaf():
     conf.timeout = IDS_WAF_CHECK_TIMEOUT
 
     try:
-        retVal = (Request.queryPage(place=place, value=value, getRatioValue=True, noteResponseTime=False, silent=True, disableTampering=True)[1] or 0) < IDS_WAF_CHECK_RATIO
+        retVal = (Request.queryPage(place=place, value=value, getRatioValue=True, noteResponseTime=False, silent=True, raise404=False, disableTampering=True)[1] or 0) < IDS_WAF_CHECK_RATIO
     except SqlmapConnectionException:
         retVal = True
     finally:
