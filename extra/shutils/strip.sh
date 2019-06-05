292c1dc91f070feda53125beb1507072ce6fe888
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-06-05 12:03:30 +0200

    Adding missing so libraries for PgSQL

--- extra/shutils/strip.sh
+++ extra/shutils/strip.sh
@@ -4,6 +4,9 @@
 #               http://www.muppetlabs.com/~breadbox/software/elfkickers.html
 #               https://ptspts.blogspot.hr/2013/12/how-to-make-smaller-c-and-c-binaries.html
 
+# https://github.com/BR903/ELFkickers/tree/master/sstrip
+# https://www.ubuntuupdates.org/package/core/cosmic/universe/updates/postgresql-server-dev-10
+
 # For example:
 # python ../../../../../extra/cloak/cloak.py -d -i lib_postgresqludf_sys.so_
 # ../../../../../extra/shutils/strip.sh lib_postgresqludf_sys.so
