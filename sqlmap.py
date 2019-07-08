f85abafd79b6c060ade3451e39f149a49d095d9a
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-07-08 11:55:04 +0200

    Patch for #3805

--- sqlmap.py
+++ sqlmap.py
@@ -238,7 +238,7 @@ def main():
             dataToStdout(excMsg)
             raise SystemExit
 
-        elif any(_ in excMsg for _ in ("ImportError", "Can't find file for module")):
+        elif any(_ in excMsg for _ in ("ImportError", "ModuleNotFoundError", "Can't find file for module")):
             errMsg = "invalid runtime environment ('%s')" % excMsg.split("Error: ")[-1].strip()
             logger.critical(errMsg)
             raise SystemExit
