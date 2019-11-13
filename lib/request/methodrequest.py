77312a8a2edebec9ee99c256388971ac1524c447
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-11-13 11:29:42 +0100

    Fixes #4010

--- lib/request/methodrequest.py
+++ lib/request/methodrequest.py
@@ -5,6 +5,7 @@ Copyright (c) 2006-2019 sqlmap developers (http://sqlmap.org/)
 See the file 'LICENSE' for copying permission
 """
 
+from lib.core.convert import getText
 from thirdparty.six.moves import urllib as _urllib
 
 class MethodRequest(_urllib.request.Request):
@@ -13,8 +14,7 @@ class MethodRequest(_urllib.request.Request):
     """
 
     def set_method(self, method):
-        self.method = method.upper()
-        print("%s,%s" % (type(self.method), type(self.data)))
+        self.method = getText(method.upper())  # Dirty hack for Python3 (may it rot in hell!)
 
     def get_method(self):
         return getattr(self, 'method', _urllib.request.Request.get_method(self))
