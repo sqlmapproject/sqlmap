f550a2281fe531a510d14457d3f1efb799d0bb3f
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-11-17 22:14:32 +0100

    Minor patch (SQLite --tables)

--- tamper/randomcase.py
+++ tamper/randomcase.py
@@ -51,7 +51,7 @@ def tamper(payload, **kwargs):
         for match in re.finditer(r"\b[A-Za-z_]{2,}\b", retVal):
             word = match.group()
 
-            if (word.upper() in kb.keywords and re.search(r"(?i)[`\"\[]%s[`\"\]]" % word, retVal) is None) or ("%s(" % word) in payload:
+            if (word.upper() in kb.keywords and re.search(r"(?i)[`\"'\[]%s[`\"'\]]" % word, retVal) is None) or ("%s(" % word) in payload:
                 while True:
                     _ = ""
 
