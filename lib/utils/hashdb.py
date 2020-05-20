ddf8b1b198b6d73156cbd38b22b11c42142793a8
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-05-20 16:11:51 +0200

    Fixes #4208

--- lib/utils/hashdb.py
+++ lib/utils/hashdb.py
@@ -68,7 +68,7 @@ class HashDB(object):
 
     @staticmethod
     def hashKey(key):
-        key = getBytes(key if isinstance(key, six.text_type) else repr(key))
+        key = getBytes(key if isinstance(key, six.text_type) else repr(key), errors="xmlcharrefreplace")
         retVal = int(hashlib.md5(key).hexdigest(), 16) & 0x7fffffffffffffff  # Reference: http://stackoverflow.com/a/4448400
         return retVal
 
