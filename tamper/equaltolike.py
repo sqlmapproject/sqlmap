bcfd9c3f48f1561ba4e29dd248987a8aa7490685
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-08-04 10:27:52 +0200

    Trivial update

--- tamper/equaltolike.py
+++ tamper/equaltolike.py
@@ -15,7 +15,7 @@ from lib.core.enums import PRIORITY
 __priority__ = PRIORITY.HIGHEST
 
 def dependencies():
-    singleTimeWarnMessage("tamper script '%s' is unlikely to work against %s" % (os.path.basename(__file__).split(".")[0], DBMS.PGSQL))
+    pass
 
 def tamper(payload, **kwargs):
     """
