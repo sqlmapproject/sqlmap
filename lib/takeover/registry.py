cfe9fb4f5b3d45c46608bf080ee203686c6a89ee
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-08-10 21:27:38 +0200

    Fixes #4301

--- lib/takeover/registry.py
+++ lib/takeover/registry.py
@@ -7,6 +7,7 @@ See the file 'LICENSE' for copying permission
 
 import os
 
+from lib.core.common import openFile
 from lib.core.common import randomStr
 from lib.core.data import conf
 from lib.core.data import logger
@@ -48,7 +49,7 @@ class Registry(object):
         )
 
     def _createLocalBatchFile(self):
-        self._batPathFp = open(self._batPathLocal, "w")
+        self._batPathFp = openFile(self._batPathLocal, "w")
 
         if self._operation == REGISTRY_OPERATION.READ:
             lines = self._batRead
