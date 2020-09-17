d51e45fd3428370f22576080c77da1f5e03b046a
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-09-17 15:26:06 +0200

    Minor update for #4344

--- tamper/sleep2getlock.py
+++ tamper/sleep2getlock.py
@@ -5,7 +5,6 @@ Copyright (c) 2006-2020 sqlmap developers (http://sqlmap.org/)
 See the file 'LICENSE' for copying permission
 """
 
-from lib.core.compat import xrange
 from lib.core.data import kb
 from lib.core.enums import PRIORITY
 
@@ -16,7 +15,7 @@ def dependencies():
 
 def tamper(payload, **kwargs):
     """
-    Replaces instances like 'SLEEP(5)' with (e.g.) "get_lock('ETgP',5)"
+    Replaces instances like 'SLEEP(5)' with (e.g.) "GET_LOCK('ETgP',5)"
 
     Requirement:
         * MySQL
@@ -30,11 +29,11 @@ def tamper(payload, **kwargs):
 
         * Reference: https://zhuanlan.zhihu.com/p/35245598
 
-    >>> tamper('SLEEP(5)') == "get_lock('%s',5)" % kb.aliasName
+    >>> tamper('SLEEP(5)') == "GET_LOCK('%s',5)" % kb.aliasName
     True
     """
 
     if payload:
-        payload = payload.replace("SLEEP(", "get_lock('%s'," % kb.aliasName)
+        payload = payload.replace("SLEEP(", "GET_LOCK('%s'," % kb.aliasName)
 
     return payload
