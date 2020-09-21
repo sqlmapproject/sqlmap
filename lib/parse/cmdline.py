5d08b9004eedd50f9516f6d67b267e78bf619a85
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-09-21 17:11:11 +0200

    Minor update

--- lib/parse/cmdline.py
+++ lib/parse/cmdline.py
@@ -683,10 +683,10 @@ def cmdLineParser(argv=None):
             help="Parse and display DBMS error messages from responses")
 
         general.add_argument("--preprocess", dest="preprocess",
-            help="Use given script(s) for preprocessing (request data)")
+            help="Use given script(s) for preprocessing (request)")
 
         general.add_argument("--postprocess", dest="postprocess",
-            help="Use given script(s) for postprocessing (response data)")
+            help="Use given script(s) for postprocessing (response)")
 
         general.add_argument("--repair", dest="repair", action="store_true",
             help="Redump entries having unknown character marker (%s)" % INFERENCE_UNKNOWN_CHAR)
