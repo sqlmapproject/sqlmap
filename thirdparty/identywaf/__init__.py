82e28e038e7f4124ff9ceb069658ecf7b53e7318
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-12-31 11:49:19 +0100

    Another year bump

--- thirdparty/identywaf/__init__.py
+++ thirdparty/identywaf/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2019-2020 Miroslav Stampar (@stamparm), MIT
+# Copyright (c) 2019-2021 Miroslav Stampar (@stamparm), MIT
 # See the file 'LICENSE' for copying permission
 
 # The above copyright notice and this permission notice shall be included in
