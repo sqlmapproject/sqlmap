86467e196bc73f41b1aeb2f87dbb6e4b606b8efe
Miroslav Stampar <miroslav.stampar@gmail.com>
2021-02-19 11:08:14 +0100

    Periodical recloak

--- extra/cloak/cloak.py
+++ extra/cloak/cloak.py
@@ -21,7 +21,7 @@ if sys.version_info >= (3, 0):
     xrange = range
     ord = lambda _: _
 
-KEY = b"cwRAopWDYixMeqs3"
+KEY = b"ENWsCymUeJcXqSbD"
 
 def xor(message, key):
     return b"".join(struct.pack('B', ord(message[i]) ^ ord(key[i % len(key)])) for i in range(len(message)))
