7c41967865eff84b57d89bb317102004a7f9d1b5
Miroslav Stampar <miroslav.stampar@gmail.com>
2021-06-21 17:17:41 +0200

    Fixes #4719

--- lib/core/replication.py
+++ lib/core/replication.py
@@ -29,7 +29,7 @@ class Replication(object):
             self.cursor = self.connection.cursor()
         except sqlite3.OperationalError as ex:
             errMsg = "error occurred while opening a replication "
-            errMsg += "file '%s' ('%s')" % (self.filepath, getSafeExString(ex))
+            errMsg += "file '%s' ('%s')" % (dbpath, getSafeExString(ex))
             raise SqlmapConnectionException(errMsg)
 
     class DataType(object):
