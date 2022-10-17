02dcf2a9264a4858b3eda41ff9cf65b44014e469
Miroslav Stampar <miroslav.stampar@gmail.com>
2022-10-17 12:21:47 +0200

    Fixes #5203

--- tamper/htmlencode.py
+++ tamper/htmlencode.py
@@ -20,6 +20,12 @@ def tamper(payload, **kwargs):
 
     >>> tamper("1' AND SLEEP(5)#")
     '1&#39;&#32;AND&#32;SLEEP&#40;5&#41;&#35;'
+    >>> tamper("1&#39;&#32;AND&#32;SLEEP&#40;5&#41;&#35;")
+    '1&#39;&#32;AND&#32;SLEEP&#40;5&#41;&#35;'
     """
 
-    return re.sub(r"[^\w]", lambda match: "&#%d;" % ord(match.group(0)), payload) if payload else payload
+    if payload:
+        payload = re.sub(r"&#(\d+);", lambda match: chr(int(match.group(1))), payload)      # NOTE: https://github.com/sqlmapproject/sqlmap/issues/5203
+        payload = re.sub(r"[^\w]", lambda match: "&#%d;" % ord(match.group(0)), payload)
+
+    return payload
