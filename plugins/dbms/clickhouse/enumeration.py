a1bf89d31e7f018aa89ca144b074624bce60cde3
Miroslav Stampar <miroslav.stampar@gmail.com>
2023-02-24 21:51:52 +0100

    Fixes #5344

--- plugins/dbms/clickhouse/enumeration.py
+++ plugins/dbms/clickhouse/enumeration.py
@@ -5,7 +5,18 @@ Copyright (c) 2006-2023 sqlmap developers (https://sqlmap.org/)
 See the file 'LICENSE' for copying permission
 """
 
+from lib.core.data import logger
 from plugins.generic.enumeration import Enumeration as GenericEnumeration
 
 class Enumeration(GenericEnumeration):
-    pass
+    def getPasswordHashes(self):
+        warnMsg = "on ClickHouse it is not possible to enumerate the user password hashes"
+        logger.warning(warnMsg)
+
+        return {}
+
+    def getRoles(self, *args, **kwargs):
+        warnMsg = "on ClickHouse it is not possible to enumerate the user roles"
+        logger.warning(warnMsg)
+
+        return {}
