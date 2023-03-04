1f0e1a4d8480d8ce664c0c7d6509f74299c85d27
Kiki <126977631+KikiCod@users.noreply.github.com>
2023-03-05 01:27:59 +0300

    Update sqlmap.py

--- sqlmap.py
+++ sqlmap.py
@@ -2,7 +2,7 @@
 
 """
 Copyright (c) 2006-2023 sqlmap developers (https://sqlmap.org/)
-See the file 'LICENSE' for copying permission
+See the file 'LICENSE' for copying permission AI SUPPORTED
 """
 
 from __future__ import print_function
