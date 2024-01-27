d68ac1b19a43aa34f995f3dcbdcf9cfaf2c60d6b
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2024-01-26 23:47:23 -0500

    space 2htab tamper added for replacing horizontal space (%9) with spaces in payload string

---
+++ tamper/space2htab.py
@@ -0,0 +1,22 @@
+from lib.core.compat import xrange
+
+
+
+def dependencies():
+    pass
+
+def tamper(payload:str,**kwargs):
+    """
+    Replace payload space characters with horizontal space(%09)
+    >>> tamper("SELECT id FROM users")
+    'SELECT%09id%09FROM%09users'
+    """
+    retVal = payload
+    place_space = "%9"
+    if payload:
+        for i in xrange(len(payload)):
+            if payload[i].isspace():
+                rm_value = payload[i]
+                retVal = retVal.replace(rm_value, place_space)
+
+    return retVal
