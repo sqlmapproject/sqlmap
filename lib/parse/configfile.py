1e9e33d9c3743ce37fa6a6076c61d01b014658da
Miroslav Stampar <miroslav.stampar@gmail.com>
2024-04-12 17:32:38 +0200

    Proper patch for #5688

--- lib/parse/configfile.py
+++ lib/parse/configfile.py
@@ -68,7 +68,10 @@ def configFileParser(configFile):
 
     try:
         config = UnicodeRawConfigParser()
-        config.readfp(configFP)
+        if hasattr(config, "read_file"):
+            config.read_file(configFP)
+        else:
+            config.readfp(configFP)
     except Exception as ex:
         errMsg = "you have provided an invalid and/or unreadable configuration file ('%s')" % getSafeExString(ex)
         raise SqlmapSyntaxException(errMsg)
