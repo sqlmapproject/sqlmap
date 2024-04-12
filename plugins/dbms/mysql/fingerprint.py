dce99e0b40d3425d47ba8361032878fd36a56ea1
Miroslav Stampar <miroslav.stampar@gmail.com>
2024-04-12 16:21:32 +0200

    Trivial update

--- plugins/dbms/mysql/fingerprint.py
+++ plugins/dbms/mysql/fingerprint.py
@@ -45,10 +45,11 @@ class Fingerprint(GenericFingerprint):
         # Reference: https://dev.mysql.com/doc/relnotes/mysql/<major>.<minor>/en/
 
         versions = (
+            (80200, 80202),  # MySQL 8.2
             (80100, 80102),  # MySQL 8.1
-            (80000, 80035),  # MySQL 8.0
+            (80000, 80036),  # MySQL 8.0
             (60000, 60014),  # MySQL 6.0
-            (50700, 50744),  # MySQL 5.7
+            (50700, 50745),  # MySQL 5.7
             (50600, 50652),  # MySQL 5.6
             (50500, 50563),  # MySQL 5.5
             (50400, 50404),  # MySQL 5.4
