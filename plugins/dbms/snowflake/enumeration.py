37bb07394cdcd2d688e5a0f60875e04f53408302
Jaggar <jaggar@goonsecurity.com>
2025-11-18 18:59:28 -0500

    Added plugin logic for the 'Snowflake' DBMS

---
+++ plugins/dbms/snowflake/enumeration.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2025 sqlmap developers (https://sqlmap.org)
+See the file 'LICENSE' for copying permission
+"""
+
+from lib.core.data import logger
+from lib.core.exception import SqlmapUnsupportedFeatureException
+from plugins.generic.enumeration import Enumeration as GenericEnumeration
+
+class Enumeration(GenericEnumeration):
+    def getPasswordHashes(self):
+        warnMsg = "on Snowflake it is not possible to enumerate the user password hashes"
+        logger.warning(warnMsg)
+        return {}
+
+    def getHostname(self):
+        warnMsg = "on Snowflake it is not possible to enumerate the hostname"
+        logger.warning(warnMsg)
+
+    def searchDb(self):
+        warnMsg = "on Snowflake it is not possible to search databases"
+        logger.warning(warnMsg)
+        return []
+
+    def searchColumn(self):
+        errMsg = "on Snowflake it is not possible to search columns"
+        raise SqlmapUnsupportedFeatureException(errMsg)
+
+    def getPrivileges(self, *args, **kwargs):
+        warnMsg = "on SQLite it is not possible to enumerate the user privileges"
+        logger.warning(warnMsg)
+        return {}
+
+    def getStatements(self):
+        warnMsg = "on Snowflake it is not possible to enumerate the SQL statements"
+        logger.warning(warnMsg)
+        return []
