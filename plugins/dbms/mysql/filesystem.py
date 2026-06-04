0d4aff7e1ad500dadb8b2ecad8632e60fd6c6b04
Miroslav Štampar <miroslav.stampar@gmail.com>
2026-06-04 20:18:12 +0200

    Fixing MySQL stacked file reading

--- plugins/dbms/mysql/filesystem.py
+++ plugins/dbms/mysql/filesystem.py
@@ -79,7 +79,7 @@ class Filesystem(GenericFilesystem):
             if length > chunkSize:
                 result = []
 
-                for i in xrange(1, length, chunkSize):
+                for i in xrange(1, length + 1, chunkSize):
                     chunk = inject.getValue("SELECT MID(%s, %d, %d) FROM %s" % (self.tblField, i, chunkSize, self.fileTblName), unpack=False, resumeValue=False, charsetType=CHARSET_TYPE.HEXADECIMAL)
                     result.append(chunk)
             else:
